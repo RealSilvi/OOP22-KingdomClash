4925917c2109959b64987e51fbb693cfc6e968c6
RealSilvi <silvi.sinani@studio.unibo.it>
2023-04-12 20:00:40 +0200

    Removed the BotPanel and created the PlayerPanel which will be the panel for the player but also the bot. Added new methods at TroopButton to make easier the structure of his container.

--- src/main/java/it/unibo/view/battle/impl/PlayerPanel.java
+++ src/main/java/it/unibo/view/battle/impl/PlayerPanel.java
@@ -4,38 +4,95 @@ import it.unibo.view.battle.api.BattleGuiPanels;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Observable;
+import java.awt.event.ActionListener;
+import java.util.*;
+import java.util.List;
 
 public class PlayerPanel extends JPanel implements BattleGuiPanels {
 
+    private final static int NUMBER_OF_SLOTS = 5;
+    private final static double PANEL_VERTICAL_SCALE = 0.2;
+    private final static double BUTTON_SCALE = 0.12;
+
+    private final  int PREFERRED_WIDTH;
+    private final  int PREFERRED_HEIGHT;
     private final Image backgroundImage;
 
-    public PlayerPanel(Dimension screenSize) {
+
+    private List<TroopButton> slots;
+
+    public PlayerPanel(final Dimension screenSize) {
+        this.PREFERRED_WIDTH= (int) screenSize.getWidth();
+        this.PREFERRED_HEIGHT = (int) (screenSize.getHeight() * PANEL_VERTICAL_SCALE);
+        //questo path poi va preso dal model?
         this.backgroundImage = new ImageIcon(
                 "src/main/resources/it/unibo/icons/battle/battleUpDown.png"
         ).getImage();
-
         this.setOpaque(false);
+        this.setPreferredSize(new Dimension(this.getPreferredWidth(), this.getPreferredHeight()));
 
-        this.setPreferredSize(new Dimension((int)(screenSize.getWidth()), (int)(screenSize.getHeight() * 0.2)));
+        this.restart();
     }
-
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // Draw the background image.
         g.drawImage(backgroundImage, 0, 0, this);
     }
 
     @Override
     public void restart() {
+        this.slots = new ArrayList<>();
 
+        for(int i=0; i<NUMBER_OF_SLOTS; i++){
+            this.slots.add(new TroopButton());
+        }
+
+        this.slots.forEach(this::add);
+
+        this.setButtonsSize();
+
+    }
+
+    private void setButtonsSize(){
+        this.slots
+                .forEach(
+                        x -> x.setPreferredSize(new Dimension(
+                                (int)(this.PREFERRED_WIDTH * BUTTON_SCALE),
+                                (int)(this.PREFERRED_WIDTH * BUTTON_SCALE))));
     }
 
     @Override
     public void update(Observable o, Object arg) {
+        setRandomSlots();
+        this.disableSelectedSlots();
+    }
 
+    private void setRandomSlots() {
+        this.slots
+                .stream()
+                .filter(TroopButton::getSelectable)
+                .forEach(TroopButton::changeTroop);
+    }
+
+    private void disableSelectedSlots(){
+        this.slots.forEach(x -> x.setEnabled(x.getSelectable()));
+    }
+
+    public int getPreferredWidth() {
+        return this.PREFERRED_WIDTH;
+    }
+
+    public int getPreferredHeight() {
+        return this.PREFERRED_HEIGHT;
+    }
+
+    public void disableAllSlots(){
+        this.slots.forEach(x -> x.setEnabled(false));
+    }
+
+    public void setActionListenersSlot(ActionListener actionListener){
+        this.slots.forEach(x -> x.addActionListener(actionListener));
     }
 }
--- src/main/java/it/unibo/view/battle/impl/TroopButton.java
+++ src/main/java/it/unibo/view/battle/impl/TroopButton.java
@@ -1,14 +1,17 @@
 package it.unibo.view.battle.impl;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TroopButton extends JButton {
 
     private Troop troop;
+    private Boolean selectable;
 
     public TroopButton() {
         super();
         this.troop = Troop.getRandomTroop();
+        this.selectable =true;
         this.setOpaque(false);
     }
 
@@ -17,8 +20,24 @@ public class TroopButton extends JButton {
         this.setIcon(new ImageIcon(this.troop.getUrl()));
     }
 
+    public void changeSelectable(){
+        this.selectable =!this.selectable;
+        this.changeBorder();
+    }
+
     public Troop getTroop() {
         return this.troop;
     }
 
+    public Boolean getSelectable() {
+        return this.selectable;
+    }
+
+    private void changeBorder(){
+        if(this.selectable){
+            this.setBorder(BorderFactory.createEmptyBorder());
+        }else{
+            this.setBorder(BorderFactory.createLineBorder(Color.YELLOW));
+        }
+    }
 }
