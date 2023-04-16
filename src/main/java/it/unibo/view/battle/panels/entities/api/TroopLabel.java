18321fbefee12b526d700c9e61dda5a2467ee972
RealSilvi <silvi.sinani@studio.unibo.it>
2023-04-16 22:35:49 +0200

    Created the TroopLabel interface

--- src/main/java/it/unibo/view/battle/panels/entities/api/TroopLabel.java
+++ src/main/java/it/unibo/view/battle/panels/entities/api/TroopLabel.java
@@ -1,4 +1,13 @@
 package it.unibo.view.battle.panels.entities.api;
 
+/**
+ * Define the behaviour of a TroopLabel.
+ */
 public interface TroopLabel {
+
+    /**
+     * Displays an empty slot.
+     * Sets a background default icon.
+     */
+    void setEmpty();
 }
