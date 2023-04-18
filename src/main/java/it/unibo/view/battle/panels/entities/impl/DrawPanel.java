b18265961df76f87acec24d572a0aba08e63b4c6
RealSilvi <silvi.sinani@studio.unibo.it>
2023-04-18 16:07:00 +0200

    Created the DrawPanel which will be the base Panel for each panel on the BattleGui.

---
+++ src/main/java/it/unibo/view/battle/panels/entities/impl/DrawPanel.java
@@ -0,0 +1,41 @@
+package it.unibo.view.battle.panels.entities.impl;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *  A JPanel with a backgroundImage.
+ */
+public class DrawPanel extends JPanel {
+
+    private final Image backgroundImage;
+
+    /**
+     * @param backgroundImage The image to set as background.
+     */
+    public DrawPanel(Image backgroundImage) {
+        this.backgroundImage= backgroundImage;
+    }
+
+    /**
+     *
+     * @param backgroundImageIcon The ImageIcon to set as background.
+     */
+    public DrawPanel(ImageIcon backgroundImageIcon){
+        this(backgroundImageIcon.getImage());
+    }
+
+    /**
+     *
+     * @param backgroundUrl The url of the image to set as background.
+     */
+    public DrawPanel(String backgroundUrl){
+        this(new ImageIcon(backgroundUrl));
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(this.backgroundImage, 0, 0, this);
+    }
+}
