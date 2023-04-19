87f1f8a4ab1e2bf34a7ed5d0a5fd30cbcbbfc855
Lorenzo La Farciola <lorenzo.lafarciola@studio.unibo.it>
2023-04-19 16:23:56 +0200

    Created Model classes of the battle(Interface and Impl)

---
+++ src/main/java/it/unibo/model/battle/BattleModelImpl.java
@@ -0,0 +1,23 @@
+package it.unibo.model.battle;
+
+import it.unibo.view.battle.Troop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BattleModelImpl implements BattleModel{
+
+    @Override
+    public void BattlePass() {
+
+
+
+    }
+
+    @Override
+    public void BattleSpin() {
+
+
+
+    }
+}
