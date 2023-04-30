5eba2f9a1038a9a45eda12c3b4d6051e623ee1f9
Marco Francolini <marco.francolini@studio.unibo.it>
2023-05-01 00:36:21 +0200

    Modified base model to use uuids instead of integers

--- src/main/java/it/unibo/model/base/BaseModel.java
+++ src/main/java/it/unibo/model/base/BaseModel.java
@@ -12,6 +12,7 @@ import it.unibo.model.data.Resource;
 import java.awt.geom.Point2D;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.UUID;
 /**
  * Interface for the base model, it has simple functions that allows other classes to interact
  * and inspect the game data
@@ -23,32 +24,32 @@ public interface BaseModel {
      * @param type type of structure
      * @param startingLevel the starting level of the structure
      * @param cheatMode if true disables the resource cost and time wait
-     * @return integer that identifies the structure
+     * @return UUID that identifies the structure
      * @throws NotEnoughResourceException thrown when the player does not have enough resources to build this structure
      * @throws InvalidBuildingPlacementException thrown when the building position is obstructed
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public int buildStructure(final Point2D position, final BuildingTypes type, final int startingLevel, final boolean cheatMode) throws NotEnoughResourceException, InvalidBuildingPlacementException, InvalidStructureReferenceException;
+    public UUID buildStructure(final Point2D position, final BuildingTypes type, final int startingLevel, final boolean cheatMode) throws NotEnoughResourceException, InvalidBuildingPlacementException, InvalidStructureReferenceException;
     /**
      * Tries to build a structure in a given position at a given level
      * @param position placing position of the structure
      * @param type type of structure
      * @param startingLevel the starting level of the structure
-     * @return integer that identifies the structure
+     * @return UUID that identifies the structure
      * @throws NotEnoughResourceException thrown when the player does not have enough resources to build this structure
      * @throws InvalidBuildingPlacementException thrown when the building position is obstructed
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public int buildStructure(final Point2D position, final BuildingTypes type, final int startingLevel) throws NotEnoughResourceException, InvalidBuildingPlacementException, InvalidStructureReferenceException;
+    public UUID buildStructure(final Point2D position, final BuildingTypes type, final int startingLevel) throws NotEnoughResourceException, InvalidBuildingPlacementException, InvalidStructureReferenceException;
     /**
      * Tries to build a structure in a given position
      * @param position placing position of the structure
      * @param type type of structure
-     * @return integer that identifies the structure
+     * @return UUID that identifies the structure
      * @throws NotEnoughResourceException thrown when the player does not have enough resources to build this structure
      * @throws InvalidBuildingPlacementException thrown when the building position is obstructed
      */
-    public int buildStructure(final Point2D position, final BuildingTypes type) throws NotEnoughResourceException, InvalidBuildingPlacementException;
+    public UUID buildStructure(final Point2D position, final BuildingTypes type) throws NotEnoughResourceException, InvalidBuildingPlacementException;
     /**
      * If the structure exists, starts the upgrading progress or builds it
      * instantly if instabuild is true
@@ -58,7 +59,7 @@ public interface BaseModel {
      * @throws BuildingMaxedOutException thrown when the the provided building has already the maximum level allowed
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public void upgradeStructure(final int structureId, final boolean cheatMode) throws NotEnoughResourceException, BuildingMaxedOutException, InvalidStructureReferenceException;
+    public void upgradeStructure(final UUID structureId, final boolean cheatMode) throws NotEnoughResourceException, BuildingMaxedOutException, InvalidStructureReferenceException;
     /**
      * If the structure exists, starts the upgrading progress
      * @param structureId an existing structure's identifier
@@ -66,14 +67,14 @@ public interface BaseModel {
      * @throws BuildingMaxedOutException thrown when the the provided building has already the maximum level allowed
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public void upgradeStructure(final int structureId) throws NotEnoughResourceException, BuildingMaxedOutException, InvalidStructureReferenceException;
+    public void upgradeStructure(final UUID structureId) throws NotEnoughResourceException, BuildingMaxedOutException, InvalidStructureReferenceException;
     /**
      * Tries to destroy a building, giving back part of the building resources spent
      * @param structureId an existing structure's identifier
      * @return a list of recovered resources
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public List<Resource> demolishStructure(final int structureId) throws InvalidStructureReferenceException;
+    public List<Resource> demolishStructure(final UUID structureId) throws InvalidStructureReferenceException;
     /**
      * Tries to relocate an already existing structure to another location if possible
      * @param position the new position of the structure
@@ -81,32 +82,32 @@ public interface BaseModel {
      * @throws InvalidBuildingPlacementException thrown when the building position is obstructed
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public void relocateStructure(final Point2D position, final int structureId) throws InvalidBuildingPlacementException, InvalidStructureReferenceException;
+    public void relocateStructure(final Point2D position, final UUID structureId) throws InvalidBuildingPlacementException, InvalidStructureReferenceException;
     /**
      * Gets the path of the texture that represents the current structure's status
      * @param structureId an existing structure's identifier
      * @return the texture's path
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public Path getStructureTexture(final int structureId) throws InvalidStructureReferenceException;
+    public Path getStructureTexture(final UUID structureId) throws InvalidStructureReferenceException;
     /**
      * Given a structure's identifier, returns the progress in percentage of the current operation
      * @param structureId an existing structure's identifier
      * @return progress in percentage
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public int getBuildingProgress(final int structureId) throws InvalidStructureReferenceException;
+    public int getBuildingProgress(final UUID structureId) throws InvalidStructureReferenceException;
     /**
      * Produces a list of materials that the structures produces
      * @param structureId an existing structure's identifier
      * @return a list of materials that the structures produces
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public List<Resource> getBuildingProduction(final int structureId) throws InvalidStructureReferenceException;
+    public List<Resource> getBuildingProduction(final UUID structureId) throws InvalidStructureReferenceException;
     /**
      * @return an identifier for every existing building
      */
-    public List<Integer> getBuildingIds();
+    public List<UUID> getBuildingIds();
 
     /**
      * Returns the amount of the provided type of resources that the player has
