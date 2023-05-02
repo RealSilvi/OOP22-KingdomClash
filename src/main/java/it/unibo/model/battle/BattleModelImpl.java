c81b323bb40236a7d4965a0412b62a9599e85e82
Lorenzo La Farciola <lorenzo.lafarciola@studio.unibo.it>
2023-05-02 18:29:51 +0200

    Finished ordered function and battle function

--- src/main/java/it/unibo/model/battle/BattleModelImpl.java
+++ src/main/java/it/unibo/model/battle/BattleModelImpl.java
@@ -1,16 +1,30 @@
 package it.unibo.model.battle;
 
 import it.unibo.view.battle.Troop;
-import java.util.HashMap;
-import java.util.Map;
+
+import javax.swing.text.html.Option;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BattleModelImpl implements BattleModel{
 
     public static final int TOTAL_TROOPS = 10;
     public static final int PLAYER_TROOPS = 5;
     public static final int BOT_TROOPS = 5;
+    public static final int CORRISPONDED_TROOP = 0;
 
-    private final Map<Integer, Map<Troop, Boolean>> battleTroop = new HashMap<>();
+    //private final Map<Integer, Map<Troop, Boolean>> battleTroop = new HashMap<>();
+    private List<Troop> playerTroop = new ArrayList<>();
+    private List<Troop> botTroop = new ArrayList<>();
+
+    private List<Optional<Troop>> playerField = new ArrayList<>();
+    private List<Optional<Troop>> botField = new ArrayList<>();
+
+    int counted_round = 0;
+    int botLife = 10;
+
+    int playerLife = 10;
+
 
     private void Initialize(){
         Map<Troop, Boolean> buttonTroop = new HashMap<>();
@@ -44,40 +58,33 @@ public class BattleModelImpl implements BattleModel{
 
     }
 
-    @Override
-    public void ChoosenButton(Integer key){
+    private void BattleCombat(){
 
-        Troop troop = conteinedTroop(key);
+        playerField.forEach(x -> {
 
-        if((!this.battleTroop.isEmpty()) && this.battleTroop.get(key).containsValue(true)){
-            this.battleTroop.get(key).put(troop, false);
-        }else{
-            this.battleTroop.get(key).put(troop, true);
-        }
+                if(botField.get(CORRISPONDED_TROOP).isPresent() && x.isPresent()){
+                    if(x.get().getLevel() > botField.get(CORRISPONDED_TROOP).get().getLevel()){
+                        if(!x.get().isDefense()){
+                            botLife--;
+                        }
+                    }else if(x.get().getLevel() < botField.get(CORRISPONDED_TROOP).get().getLevel()){
+                        if(x.get().isDefense()){
+                            playerLife--;
+                        }
+                    }
+                }else if(botField.get(CORRISPONDED_TROOP).isEmpty() && x.isPresent() && (!x.get().isDefense())){
+                    botLife--;
+                }else if(x.isEmpty() && botField.get(CORRISPONDED_TROOP).isPresent() && (!botField.get(CORRISPONDED_TROOP).get().isDefense())){
+                    playerLife--;
+                }
 
-        //TODO notify the controller to change the status of the selected troop and the field
+                if(botField.get(CORRISPONDED_TROOP).isPresent()){
+                    botField.remove(CORRISPONDED_TROOP);
+                }
 
-    }
+        });
 
-    private Troop getRightTroop(Integer key_troop){
-
-        if(Troop.AXE.getId() == key_troop){
-            return Troop.AXE;
-        } else if (Troop.ARROW.getId() == key_troop) {
-            return Troop.ARROW;
-        }else if (Troop.HELMET.getId() == key_troop) {
-            return Troop.HELMET;
-        }else if (Troop.SHIELD.getId() == key_troop) {
-            return Troop.SHIELD;
-        }else if (Troop.DODGE.getId() == key_troop) {
-            return Troop.DODGE;
-        }else if (Troop.TOWER.getId() == key_troop) {
-            return Troop.TOWER;
-        }else if (Troop.CATAPULT.getId() == key_troop) {
-            return Troop.CATAPULT;
-        }else {
-            return Troop.SWORD;
-        }
+        playerField = null;
 
     }
 
@@ -93,5 +100,50 @@ public class BattleModelImpl implements BattleModel{
 
         return troop;
     }
+
+    private void getOrderedField(){
+        List<Optional<Troop>> playerOptionalList = new ArrayList<>();
+        List<Optional<Troop>> botOptionalList = new ArrayList<>();
+        playerField = playerOptionalList;
+        botField = botOptionalList;
+        int difference_size;
+
+        for( int i=0; i<TOTAL_TROOPS; i++){
+            int a=i;
+            playerOptionalList.addAll(playerTroop.stream().filter(x -> x.getId()==a).map(Optional::of).toList());
+            botOptionalList.addAll(botTroop.stream()
+                    .filter(x ->
+                            x.equals(Troop.getNullable(
+                                        Arrays.stream(Troop.values())
+                                                .filter(z -> z.getId() == a)
+                                                .iterator()
+                                                .next()))
+                    )
+                    .map(Optional::of)
+                    .toList());
+
+            if(playerOptionalList.size() < botOptionalList.size()){
+                difference_size = botOptionalList.size() - playerOptionalList.size();
+                for(i = 0; i < difference_size; i++ ){
+                    playerOptionalList.add(Optional.empty());
+                }
+            } else if (playerOptionalList.size() > botOptionalList.size()) {
+                difference_size = playerOptionalList.size() - botOptionalList.size();
+                for(i = 0; i < difference_size; i++ ){
+                    botOptionalList.add(Optional.empty());
+                }
+            }
+
+        }
+
+        //updateField(playerOptionalList, botOptionalList)
+
+    }
+
+    private void notifyController(){
+
+    }
+
+
 
 }
