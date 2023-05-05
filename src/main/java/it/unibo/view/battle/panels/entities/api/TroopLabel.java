4ce9e1cfb2992a5c102f5821f2afee555d8bf5f0
RealSilvi <silvi.sinani@studio.unibo.it>
2023-05-05 20:10:13 +0200

    Modified the Trooplabel and it's implementation to adapt it to the new ImageIconSupplier.

--- src/main/java/it/unibo/view/battle/panels/entities/api/TroopLabel.java
+++ src/main/java/it/unibo/view/battle/panels/entities/api/TroopLabel.java
@@ -1,13 +1,21 @@
 package it.unibo.view.battle.panels.entities.api;
 
+import it.unibo.view.battle.Troop;
+
 /**
  * Define the behaviour of a TroopLabel.
  */
 public interface TroopLabel {
 
     /**
-     * Displays an empty slot.
+     * Displays an empty field's slot.<br>
      * Sets a background default icon.
      */
     void setEmpty();
+
+    /**
+     * Displays a troop on the field's slot.
+     * @param troop the troop to display.
+     */
+    void setTroop(Troop troop);
 }
--- src/main/java/it/unibo/view/battle/panels/entities/impl/TroopLabelImpl.java
+++ src/main/java/it/unibo/view/battle/panels/entities/impl/TroopLabelImpl.java
@@ -1,21 +1,28 @@
 package it.unibo.view.battle.panels.entities.impl;
 
-import it.unibo.view.battle.panels.utilities.ImageIconEntitiesManager;
+import it.unibo.view.battle.Troop;
+import it.unibo.view.battle.panels.entities.api.TroopLabel;
+import it.unibo.view.battle.panels.utilities.ImageIconsSupplier;
 
 import javax.swing.*;
 
-public class TroopLabelImpl extends JLabel implements it.unibo.view.battle.panels.entities.api.TroopLabel {
+public class TroopLabelImpl extends JLabel implements TroopLabel {
 
-    public TroopLabelImpl(Icon image) {
-        super(image);
+    public TroopLabelImpl(Troop troop) {
+        super(ImageIconsSupplier.getImageIconFromTroop(troop,true));
     }
 
     public TroopLabelImpl() {
-        super(ImageIconEntitiesManager.getImageDefaultBackground());
+        super(ImageIconsSupplier.BACKGROUND_FREE_SPOT_URL);
     }
 
     @Override
     public void setEmpty(){
-        this.setIcon(ImageIconEntitiesManager.getImageDefaultBackground());
+        this.setIcon(ImageIconsSupplier.BACKGROUND_FREE_SPOT_URL);
+    }
+
+    @Override
+    public void setTroop(Troop troop){
+        this.setIcon(ImageIconsSupplier.getImageIconFromTroop(troop,true));
     }
 }
