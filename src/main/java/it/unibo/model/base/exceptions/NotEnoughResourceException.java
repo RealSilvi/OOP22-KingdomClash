4d73009cde52716505ab0e8263c18842dcbc052f
Marco Francolini <marco.francolini@studio.unibo.it>
2023-05-07 19:59:46 +0200

    Added constructor to NotEnoughResourcesException

--- src/main/java/it/unibo/model/base/exceptions/NotEnoughResourceException.java
+++ src/main/java/it/unibo/model/base/exceptions/NotEnoughResourceException.java
@@ -1,10 +1,23 @@
 package it.unibo.model.base.exceptions;
 
+import java.util.Set;
+
 import it.unibo.model.data.Resource;
 
 public class NotEnoughResourceException extends ResourceException {
     public NotEnoughResourceException(Resource.ResourceType resource) {
         super("Not enough "+resource.name()+"!");
+    }
+    public NotEnoughResourceException(Set<Resource> resources) {
+        super("You still need "
+        +resources
+            .stream()
+            .collect(StringBuilder::new,
+                (strb, res)->{
+                    strb.append(res.getAmount()+" "+res.getResource().name()+" ");
+                },
+                StringBuilder::append)
+        +"to build this!");
     }
     public NotEnoughResourceException(String msg) {
         super(msg);
