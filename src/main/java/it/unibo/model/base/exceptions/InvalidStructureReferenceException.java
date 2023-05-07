050173eb5c84e57da0370680876c252d0a936ed8
Marco Francolini <marco.francolini@studio.unibo.it>
2023-05-07 19:59:10 +0200

    Updated exception to use uuid instead of integer

--- src/main/java/it/unibo/model/base/exceptions/InvalidStructureReferenceException.java
+++ src/main/java/it/unibo/model/base/exceptions/InvalidStructureReferenceException.java
@@ -1,8 +1,10 @@
 package it.unibo.model.base.exceptions;
 
+import java.util.UUID;
+
 public class InvalidStructureReferenceException extends BuildingException {
-    public InvalidStructureReferenceException(int identifier) {
-        super("Reference for structure identifier: "+identifier+" does not exist!");
+    public InvalidStructureReferenceException(UUID identifier) {
+        super("Reference for structure identifier: "+identifier.toString()+" does not exist!");
     }
     public InvalidStructureReferenceException(String msg) {
         super(msg);
