9a36cb8df43f5365519246c11f9566b1f386986f
Marco Francolini <marco.francolini@studio.unibo.it>
2023-05-12 17:35:48 +0200

    Removed unused function and written documentation

--- src/main/java/it/unibo/model/base/basedata/Building.java
+++ src/main/java/it/unibo/model/base/basedata/Building.java
@@ -1,8 +1,5 @@
 package it.unibo.model.base.basedata;
 
-import java.net.URL;
-import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 import it.unibo.model.base.internal.BuildingBuilder.BuildingTypes;
@@ -10,89 +7,151 @@ import it.unibo.model.data.Resource;
 
 import java.awt.geom.Point2D;
 
+/**
+ * A simple data class that stores information about a building in the game
+ */
 public class Building {
+    /**
+     * Maximum level that the buildings can reach
+     */
     public static final int MAXLEVEL = 3;
+    /**
+     * The tax as a percentage that gets applied when reimboursing materials after demolition
+     */
     public static final int REFUND_TAX_PERCENTAGE = 25;
-    public static final String CONSTRUCTION_OVERLAY = "/resources/images/image.png";
-
-    private Optional<URL> buildingOverlay;             /*Texture to use while building structure*/
-    private List<URL> buildingTextures;                /*1 texture for every level*/
 
     private BuildingTypes type;
     private int level;
-    private float buildingTime;                        /*milliseconds*/
+    private int buildingTime;                        /*milliseconds*/
     private boolean beingBuilt;
     private int buildingProgess;
+    private int productionProgress;
     private Point2D structurePos;
     private Set<Resource> productionAmount;
 
     //The high parameter count is necessary to set all of the properties of the class
     @SuppressWarnings("java:S107")
-    public Building(Optional<URL> buildingOverlay, List<URL> buildingTextures, BuildingTypes type, int level,
-            float buildingTime, boolean beingBuilt, int buildingProgess, Point2D structurePos,
+    public Building(BuildingTypes type, int level, int buildingTime,
+            boolean beingBuilt, int buildingProgess, int productionProgress, Point2D structurePos,
             Set<Resource> productionAmount) {
-        this.buildingOverlay = buildingOverlay;
-        this.buildingTextures = buildingTextures;
         this.type = type;
         this.level = level;
         this.buildingTime = buildingTime;
         this.beingBuilt = beingBuilt;
         this.buildingProgess = buildingProgess;
+        this.productionProgress = productionProgress;
         this.structurePos = structurePos;
         this.productionAmount = productionAmount;
     }
-
-    public synchronized Optional<URL> getBuildingOverlay() {
-        return buildingOverlay;
-    }
-    public synchronized void setBuildingOverlay(Optional<URL> buildingOverlay) {
-        this.buildingOverlay = buildingOverlay;
-    }
-    public synchronized List<URL> getBuildingTextures() {
-        return buildingTextures;
-    }
-    public synchronized void setBuildingTextures(List<URL> buildingTextures) {
-        this.buildingTextures = buildingTextures;
-    }
+    /**
+     * Returns the type of building, thread safe
+     * @return the type of building
+     * @see {@link it.unibo.model.base.internal.BuildingBuilder.BuildingTypes}
+     */
     public synchronized BuildingTypes getType() {
         return type;
     }
+    /**
+     * Sets the type of building, thread safe
+     * @see {@link it.unibo.model.base.internal.BuildingBuilder.BuildingTypes}
+     */
     public synchronized void setType(BuildingTypes type) {
         this.type = type;
     }
+    /**
+     * Returns the level of the building, thread safe
+     * @return the building's current level
+     */
     public synchronized int getLevel() {
         return level;
     }
+    /**
+     * Sets the level of the building, thread safe
+     */
     public synchronized void setLevel(int level) {
         this.level = level;
     }
-    public synchronized float getBuildingTime() {
+    /**
+     * Returns the time to build the structure
+     * @return building time in milliseconds
+     */
+    public synchronized int getBuildingTime() {
         return buildingTime;
     }
-    public synchronized void setBuildingTime(float buildingTime) {
+    /**
+     * Sets the time to build the structure, thread safe
+     */
+    public synchronized void setBuildingTime(int buildingTime) {
         this.buildingTime = buildingTime;
     }
+    /**
+     * 
+     * Checks if a structure is currently being built
+     * @return true if it is being built
+     */
     public synchronized boolean isBeingBuilt() {
         return beingBuilt;
     }
+    /**
+     * Sets if a building is currently being built
+     * @param beingBuilt true if it is being built
+     */
     public synchronized void setBeingBuilt(boolean beingBuilt) {
         this.beingBuilt = beingBuilt;
     }
+    /**
+     * Gets the building progress as a percentage
+     * @return an integer that represents a percentage
+     */
     public synchronized int getBuildingProgess() {
         return buildingProgess;
     }
+    /**
+     * Sets the building progress as a percentage
+     * @param buildingProgess an integer representing a percentage
+     */
     public synchronized void setBuildingProgess(int buildingProgess) {
         this.buildingProgess = buildingProgess;
     }
+    /**
+     * Returns the progress for creating a set of resources
+     * @return an integer that represents the progress as a percentage
+     */
+    public synchronized int getProductionProgress() {
+        return productionProgress;
+    }
+    /**
+     * Set the progress for creating a set of resources
+     * @param productionProgress an integer that represents the progress as a percentage
+     */
+    public synchronized void setProductionProgress(int productionProgress) {
+        this.productionProgress = productionProgress;
+    }
+    /**
+     * Gets the current position of the building
+     * @return a Point2D that represents the building's current location
+     */
     public synchronized Point2D getStructurePos() {
         return structurePos;
     }
+    /**
+     * Sets the position of the building
+     * @param structurePos a Point2D that represents the building's location
+     */
     public synchronized void setStructurePos(Point2D structurePos) {
         this.structurePos = structurePos;
     }
+    /**
+     * Gets a list of resources produced every production cycle
+     * @return a set of resources produced
+     */
     public synchronized Set<Resource> getProductionAmount() {
         return productionAmount;
     }
+    /**
+     * Sets the list of resources that the building will produce every production cycle
+     * @param productionAmount the resources that will be produced
+     */
     public synchronized void setProductionAmount(Set<Resource> productionAmount) {
         this.productionAmount = productionAmount;
     }   
