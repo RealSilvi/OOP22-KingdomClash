9516535846f3d1de11821ff3373d139516a0123d
Lorenzo La Farciola <lorenzo.lafarciola@studio.unibo.it>
2023-05-13 17:42:20 +0200

    Created FightData and implemented some methods

---
+++ src/main/java/it/unibo/model/data/FightData.java
@@ -0,0 +1,38 @@
+package it.unibo.model.data;
+
+import it.unibo.model.battle.entitydata.BotData;
+import it.unibo.model.battle.entitydata.BotDataImpl;
+import it.unibo.model.battle.entitydata.PlayerData;
+import it.unibo.model.battle.entitydata.PlayerDataImpl;
+
+public class FightData {
+
+    public static final int BOT_TROOPS = BotDataImpl.BOT_TROOPS;
+
+    private BotData botData;
+    private PlayerData playerData;
+
+    public FightData() {
+        this.botData = new BotDataImpl();
+        this.playerData = new PlayerDataImpl();
+    }
+
+    public void setPlayerData(PlayerData playerData){
+        this.playerData = playerData;
+    }
+
+    public void setBotData(BotData botData) {
+        this.botData = botData;
+    }
+
+    public PlayerData getPlayerData() {
+        return this.playerData;
+    }
+
+    public BotData getBotData(){
+        return this.botData;
+    }
+
+
+
+}
