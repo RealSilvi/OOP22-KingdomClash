934da1d26da86f433f2e0af482f3ead90710f69f
Lorenzo La Farciola <lorenzo.lafarciola@studio.unibo.it>
2023-05-13 18:45:49 +0200

    Added documentations in each method.

--- src/main/java/it/unibo/model/battle/entitydata/PlayerData.java
+++ src/main/java/it/unibo/model/battle/entitydata/PlayerData.java
@@ -9,19 +9,63 @@ import java.util.Optional;
 
 public interface PlayerData {
 
+    /**
+     * Adds the clicked troop into the field
+     * @param key represents the position of the clicked troop
+     */
+
     public void AddPlayerTroop(Integer key);
+
+    /**
+     * Removes the clicked troop from the field
+     * @param key represents the position of the clicked troop
+     */
 
     public void RemovePlayerTroop(Integer key);
 
+    /**
+     * Selects only the clicked troops.
+     * @return the selected troops. It means all the troops in the field.
+     */
+
     public List<Troop> getSelected();
+
+    /**
+     * Selects all the troops chosen.
+     * @return all the chosen troops, that is,
+     * only the troops that cannot be taken back in hand.
+     */
 
     public List<Troop> getChosen();
 
+    /**
+     * Change all the troops in the hand, which are not clicked.
+     * @return a Map of the troop with the current values (eventually modified) and the right position.
+     */
+
     public Map<Integer, CellsImpl> changeNotSelectedTroop();
+
+    /**
+     * Blocks the clicked troops, setting them to chosen.
+     */
 
     public void setClickedToChosen();
 
+    /**
+     * Orders the field. Puts each troop against nothing or against the opposite,
+     * creating a corrected field, with empty spaces and right troops.
+     * @param botData the data of the bot,
+     * and therefore the corresponding troops chosen,
+     * which we want to compare with those of the player
+     * @return the corrected player's field.
+     */
+
     public List<Optional<Troop>> getOrderedField(BotData botData);
+
+    /**
+     * Blocks all the troops, clicked and not clicked, setting them to chosen.
+     * Chosen means immutable, non-modifiable. You can take it back to your hand.
+     */
 
     public void setAllChosen();
 
