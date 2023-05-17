96bae78a246837700d2f5e1c1c56c7af3f74c9c8
Lorenzo La Farciola <lorenzo.lafarciola@studio.unibo.it>
2023-05-17 21:11:14 +0200

    Modified implementations of the controller. Removed observer.

--- src/main/java/it/unibo/controller/battle/BattleController.java
+++ src/main/java/it/unibo/controller/battle/BattleController.java
@@ -2,22 +2,12 @@ package it.unibo.controller.battle;
 
 public interface BattleController {
 
-    public void addSubEvent(Event event, SubEvent subevent);
-
-    public void removeSubEvent(Event event, SubEvent subevent);
-
-    public void notify(Event event);
-
     public void Pass();
 
     public void Spin();
 
     public void ClickedButtonPlayer(Integer key);
 
-    public void UnClickedButtonPlayer(Integer key);
-
     public void ClickedButtonBot(Integer key);
-
-    public void UnClickedButtonBot(Integer key);
 
 }
--- src/main/java/it/unibo/controller/battle/BattleControllerImpl.java
+++ src/main/java/it/unibo/controller/battle/BattleControllerImpl.java
@@ -4,75 +4,68 @@ import it.unibo.model.battle.BattleModel;
 import it.unibo.model.battle.BattleModelImpl;
 import it.unibo.model.data.FightData;
 import it.unibo.model.data.GameData;
+import it.unibo.view.battle.BattlePanel;
+import it.unibo.view.battle.BattlePanelImpl;
 
 import java.util.*;
 
 public class BattleControllerImpl implements  BattleController{
 
-    private final Map<Event, List<SubEvent>> specific_event;
+    public static final int PLAYER = 1;
     private final BattleModel battleModel;
     private Optional<FightData> fightData;
+    private final BattlePanel battlePanel;
 
     public BattleControllerImpl(BattleModel battleModel, GameData gameData){
-        this.specific_event = new HashMap<>();
-        for(Event event : Event.values()){
-            this.specific_event.put(event,new ArrayList<>());
-        }
         this.battleModel = battleModel;
+        this.battlePanel = new BattlePanelImpl();
         if(gameData.getFightData().isPresent()){
             this.fightData = gameData.getFightData();
         }
     }
 
     public BattleControllerImpl(GameData gameData){
-        this.specific_event = new HashMap<>();
-        for(Event event : Event.values()){
-            this.specific_event.put(event,new ArrayList<>());
-        }
         this.battleModel = new BattleModelImpl(gameData);
+        this.battlePanel = new BattlePanelImpl();
         if(gameData.getFightData().isPresent()){
             this.fightData = gameData.getFightData();
         }
     }
 
-    @Override
-    public void addSubEvent(Event event, SubEvent subevent) {
-        specific_event.get(event).add(subevent);
-    }
-
-    @Override
-    public void removeSubEvent(Event event, SubEvent subevent) {
-        specific_event.get(event).remove(subevent);
-    }
-
-    @Override
-    public void notify(Event event) {
-        specific_event.get(event).forEach(x -> x.update(event));
-    }
 
     public void Pass(){
-        battleModel.BattlePass();
+        this.battlePanel.enableBotSlots();
+        this.battlePanel.disablePassButton();
+        this.battlePanel.disablePlayerSlots();
+        this.battlePanel.disableSpinButton();
+        this.battleModel.BattlePass();
+        this.battlePanel.enablePassButton();
+        this.battlePanel.enablePlayerSlots();
+        this.battlePanel.enableSpinButton();
+        this.battlePanel.disableBotSlots();
     }
 
     public void Spin(){
-        battleModel.BattleSpin();
+        this.battlePanel.disableSpinButton();
+        this.battlePanel.spinPlayerFreeSlot(battleModel.BattleSpin(PLAYER));
     }
 
     public void ClickedButtonPlayer(Integer key){
-        fightData.get().getPlayerData().AddPlayerTroop(key);
-    }
-
-    public void UnClickedButtonPlayer(Integer key){
-        fightData.get().getPlayerData().RemovePlayerTroop(key);
+        if(fightData.get().getPlayerData().getCells(key).getClicked()){
+            fightData.get().getPlayerData().RemovePlayerTroop(key);
+        }else{
+            fightData.get().getPlayerData().AddPlayerTroop(key);
+        }
     }
 
     public void ClickedButtonBot(Integer key){
-        fightData.get().getBotData().AddBotTroop(key);
+        if(fightData.get().getBotData().getCells(key).getClicked()){
+            fightData.get().getBotData().RemoveBotTroop(key);
+        }else{
+            fightData.get().getBotData().AddBotTroop(key);
+        }
     }
 
-    public void UnClickedButtonBot(Integer key){
-        fightData.get().getBotData().RemoveBotTroop(key);
-    }
 
 
 
