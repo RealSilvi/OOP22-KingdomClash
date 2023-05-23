a3488496f9b693fd3f579a80c461494049989b36
Marco Francolini <marco.francolini@studio.unibo.it>
2023-05-23 16:28:03 +0200

    Optimized BuildingBuilderImpl, fixed error

--- src/main/java/it/unibo/model/base/internal/BuildingBuilderImpl.java
+++ src/main/java/it/unibo/model/base/internal/BuildingBuilderImpl.java
@@ -1,22 +1,37 @@
 package it.unibo.model.base.internal;
 
 import java.awt.geom.Point2D;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import it.unibo.model.base.basedata.Building;
 import it.unibo.model.data.Resource;
 
 public class BuildingBuilderImpl implements BuildingBuilder {
+    private Map<BuildingTypes, Map<Integer, Building>> cache;
+
+    public BuildingBuilderImpl() {
+        this.cache = new EnumMap<>(BuildingTypes.class);
+        this.cache.forEach((buildingType, levelBuildingMap)->new HashMap<>());
+    }
+
     @Override
     public Building makeStandardBuilding(BuildingTypes type, Point2D position, int level) {
-        return new Building(type,
-        1,
+        if (cache.get(type).containsKey(level)) {
+            return cache.get(type).get(level);
+        }
+        Building standardizedBuilding = new Building(type,
+        level,
         type.getBuildTime(),
         false,
         0,
         0,
         position,
         type.getBaseProduction().stream().map(x->new Resource(x.getResource(), x.getAmount()*level)).collect(Collectors.toSet()));
+        cache.get(type).put(level, standardizedBuilding);
+        return standardizedBuilding;
     }
 
     @Override
