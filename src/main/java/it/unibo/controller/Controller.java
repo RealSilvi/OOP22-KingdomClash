6428ea0d22828496bedb650d4eb4f6bb7a00fe64
Marco Francolini <marco.francolini@studio.unibo.it>
2023-05-30 22:52:44 +0200

    Created generic controller interface

--- src/main/java/it/unibo/controller/Controller.java
+++ src/main/java/it/unibo/controller/Controller.java
@@ -1,4 +1,20 @@
 package it.unibo.controller;
 
-public class Controller {
+/**
+ * A very simple standardized controller interface
+ */
+public interface Controller {
+    /**
+     * Tells the controller to activate/deactivate all time-related logic
+     * @param currentControllerActive true for activation, false for deactivation
+     */
+    public void setActive(boolean currentControllerActive);
+    /**
+     * @return true if time related logic is active
+     */
+    public boolean isActive();
+    /**
+     * Tells the controller to safely close and stop everything it's doing
+     */
+    public void disable();
 }
