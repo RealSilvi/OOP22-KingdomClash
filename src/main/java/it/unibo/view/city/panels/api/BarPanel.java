cadfcb6438d8ed94dec20d0cd35083c88c883322
Abdoulaye Kane <abdoulaye.kane@studio.unibo.it>
2023-06-01 11:14:42 +0200

    create the interface of the menu bar for the cityGUI

---
+++ src/main/java/it/unibo/view/city/panels/api/BarPanel.java
@@ -0,0 +1,19 @@
+package it.unibo.view.city.panels.api;
+
+import java.awt.*;
+import java.awt.List;
+import java.util.*;
+
+import it.unibo.model.data.GameData;
+import it.unibo.model.data.Resource;
+import it.unibo.view.battle.Troop;
+
+public interface BarPanel {
+
+    void switchbutton();
+
+    void getTroopInfo(Set<Troop> type);
+
+    void getPlayerInfo( GameData gameData);
+
+}
