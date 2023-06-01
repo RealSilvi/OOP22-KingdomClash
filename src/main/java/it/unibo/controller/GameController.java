608af0019d1c3c6bc8d3e80c77059af446ef4181
Marco Francolini <marco.francolini@studio.unibo.it>
2023-06-01 14:43:46 +0200

    Started work GameController

---
+++ src/main/java/it/unibo/controller/GameController.java
@@ -0,0 +1,32 @@
+package it.unibo.controller;
+
+import java.util.logging.Logger;
+
+import it.unibo.model.GameModel;
+import it.unibo.view.menu.GameMenu;
+
+public class GameController {
+    public enum GameStates {
+        MENU,
+        BASE,
+        MAP,
+        BATTLE
+    }
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    private GameModel gameModel;
+    private GameMenu gameMenu;
+
+    private Controller baseController;
+    private Controller battleController;
+
+    public GameController(GameModel gameModel, GameMenu gameMenu) {
+        this.gameModel = gameModel;
+        this.gameMenu = gameMenu;        
+    }
+    
+    public void setGameState(GameStates gameState) {
+
+    }
+}
