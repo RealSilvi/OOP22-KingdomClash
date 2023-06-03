41ec4aaeb672e433662baa65c0712e523a6080fb
Marco Francolini <marco.francolini@studio.unibo.it>
2023-06-03 20:14:22 +0200

    Fixed 187 checkstyle errors

--- src/main/java/it/unibo/model/base/BaseModel.java
+++ src/main/java/it/unibo/model/base/BaseModel.java
@@ -22,16 +22,25 @@ import java.util.UUID;
 
 /**
  * Interface for the base model, it has simple functions that allows other classes to interact
- * and inspect the game data
+ * and inspect the game data.
  */
 public interface BaseModel {
-    public enum OperationType {
+    /**
+     * Defines the type of operation to apply to sets.
+     */
+    enum OperationType {
+        /**
+         * Negates the subtractings set and adds it to the other set.
+         */
         SUBTRACTION,
+        /**
+         * Sums two different sets without inverting any sign.
+         */
         ADDITION
     }
 
     /**
-     * Tries to build a structure in a given position at a given level
+     * Tries to build a structure in a given position at a given level.
      *
      * @param position      placing position of the structure
      * @param type          type of structure
@@ -42,10 +51,13 @@ public interface BaseModel {
      * @throws InvalidBuildingPlacementException  thrown when the building position is obstructed
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public UUID buildStructure(final Point2D position, final BuildingTypes type, final int startingLevel, final boolean cheatMode) throws NotEnoughResourceException, InvalidBuildingPlacementException, MaxBuildingLimitReachedException;
+    UUID buildStructure(Point2D position, BuildingTypes type,
+        int startingLevel, boolean cheatMode)
+        throws NotEnoughResourceException, InvalidBuildingPlacementException,
+        MaxBuildingLimitReachedException;
 
     /**
-     * Tries to build a structure in a given position at a given level
+     * Tries to build a structure in a given position at a given level.
      *
      * @param position      placing position of the structure
      * @param type          type of structure
@@ -55,10 +67,12 @@ public interface BaseModel {
      * @throws InvalidBuildingPlacementException  thrown when the building position is obstructed
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public UUID buildStructure(final Point2D position, final BuildingTypes type, final int startingLevel) throws NotEnoughResourceException, InvalidBuildingPlacementException, MaxBuildingLimitReachedException;
+    UUID buildStructure(Point2D position, BuildingTypes type,
+        int startingLevel) throws NotEnoughResourceException,
+        InvalidBuildingPlacementException, MaxBuildingLimitReachedException;
 
     /**
-     * Tries to build a structure in a given position
+     * Tries to build a structure in a given position.
      *
      * @param position placing position of the structure
      * @param type     type of structure
@@ -66,11 +80,13 @@ public interface BaseModel {
      * @throws NotEnoughResourceException        thrown when the player does not have enough resources to build this structure
      * @throws InvalidBuildingPlacementException thrown when the building position is obstructed
      */
-    public UUID buildStructure(final Point2D position, final BuildingTypes type) throws NotEnoughResourceException, InvalidBuildingPlacementException, MaxBuildingLimitReachedException;
+    UUID buildStructure(Point2D position, BuildingTypes type)
+        throws NotEnoughResourceException, InvalidBuildingPlacementException,
+        MaxBuildingLimitReachedException;
 
     /**
      * If the structure exists, starts the upgrading progress or builds it
-     * instantly if instabuild is true
+     * instantly if instabuild is true.
      *
      * @param structureId an existing structure's identifier
      * @param cheatMode   if true disables the resource cost and time wait
@@ -78,39 +94,45 @@ public interface BaseModel {
      * @throws BuildingMaxedOutException          thrown when the the provided building has already the maximum level allowed
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public void upgradeStructure(final UUID structureId, final boolean cheatMode) throws NotEnoughResourceException, BuildingMaxedOutException, InvalidStructureReferenceException;
+    void upgradeStructure(UUID structureId, boolean cheatMode)
+        throws NotEnoughResourceException, BuildingMaxedOutException,
+        InvalidStructureReferenceException;
 
     /**
-     * If the structure exists, starts the upgrading progress
+     * If the structure exists, starts the upgrading progress.
      *
      * @param structureId an existing structure's identifier
      * @throws NotEnoughResourceException         thrown when the player does not have enough resources to upgrade this structure
      * @throws BuildingMaxedOutException          thrown when the the provided building has already the maximum level allowed
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public void upgradeStructure(final UUID structureId) throws NotEnoughResourceException, BuildingMaxedOutException, InvalidStructureReferenceException;
+    void upgradeStructure(UUID structureId)
+        throws NotEnoughResourceException, BuildingMaxedOutException,
+        InvalidStructureReferenceException;
 
     /**
-     * Tries to destroy a building, giving back part of the building resources spent
+     * Tries to destroy a building, giving back part of the building resources spent.
      *
      * @param structureId an existing structure's identifier
      * @return a set of recovered resources
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public Set<Resource> demolishStructure(final UUID structureId) throws InvalidStructureReferenceException;
+    Set<Resource> demolishStructure(UUID structureId)
+        throws InvalidStructureReferenceException;
 
     /**
-     * Tries to relocate an already existing structure to another location if possible
+     * Tries to relocate an already existing structure to another location if possible.
      *
      * @param position    the new position of the structure
      * @param structureId an existing structure's identifier
      * @throws InvalidBuildingPlacementException  thrown when the building position is obstructed
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public void relocateStructure(final Point2D position, final UUID structureId) throws InvalidBuildingPlacementException, InvalidStructureReferenceException;
+    void relocateStructure(Point2D position, UUID structureId)
+        throws InvalidBuildingPlacementException, InvalidStructureReferenceException;
 
     /**
-     * Gets the path of the texture that represents the current structure's status
+     * Gets the path of the texture that represents the current structure's status.
      *
      * @param structureId an existing structure's identifier
      * @return the texture's path
@@ -119,186 +141,192 @@ public interface BaseModel {
      * responsability for the model anymore
      */
     @Deprecated
-    public Path getStructureTexture(final UUID structureId) throws InvalidStructureReferenceException;
+    Path getStructureTexture(UUID structureId)
+        throws InvalidStructureReferenceException;
 
     /**
-     * Given a structure's identifier, returns the progress in percentage of the current operation
+     * Given a structure's identifier, returns the progress in percentage of the current operation.
      *
      * @param structureId an existing structure's identifier
      * @return progress in percentage
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public int getBuildingProgress(final UUID structureId) throws InvalidStructureReferenceException;
+    int getBuildingProgress(UUID structureId)
+        throws InvalidStructureReferenceException;
 
     /**
-     * Produces a set of materials that the structures produces
+     * Produces a set of materials that the structures produces.
      *
      * @param structureId an existing structure's identifier
      * @return a set of materials that the structures produces
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public Set<Resource> getBuildingProduction(final UUID structureId) throws InvalidStructureReferenceException;
+    Set<Resource> getBuildingProduction(UUID structureId)
+        throws InvalidStructureReferenceException;
 
     /**
-     * Checks if the current structure is being built and returns a boolean
+     * Checks if the current structure is being built and returns a boolean.
      *
      * @param structureId an existing structure's identifier
      * @return true if the structure is currently being built
      * @throws InvalidStructureReferenceException thrown when the provided identifier does not represent a building
      */
-    public boolean isBuildingBeingBuilt(UUID structureId) throws InvalidStructureReferenceException;
+    boolean isBuildingBeingBuilt(UUID structureId)
+        throws InvalidStructureReferenceException;
 
     /**
      * @return an identifier for every existing building
      */
-    public Set<UUID> getBuildingIds();
+    Set<UUID> getBuildingIds();
 
     /**
-     * Returns the amount of the provided type of resources that the player has
+     * Returns the amount of the provided type of resources that the player has.
      *
      * @param type the type of resource to query for
      * @return the amount of the type of resource
      */
-    public int getResourceCount(final Resource.ResourceType type);
+    int getResourceCount(Resource.ResourceType type);
 
     /**
-     * For every existing resource type, returns the amount that the player has in an unmodifiable set
+     * For every existing resource type, returns the amount that the player has in an unmodifiable set.
      *
      * @return an unmodifiable set of resources
      */
-    public Set<Resource> getResourceCount();
+    Set<Resource> getResourceCount();
 
     /**
      * @param troopToUpgrade
      */
-    public void upgradeTroop(TroopType troopToUpgrade) throws InvalidTroopLevelException;
+    void upgradeTroop(TroopType troopToUpgrade)
+        throws InvalidTroopLevelException;
 
     /**
-     * Upgrades a troop to a given level
+     * Upgrades a troop to a given level.
      *
      * @param troopToUpgrade the type of troop to upgrade
      * @param level          the level wich the troop has to be upgraded to
      */
-    public void upgradeTroop(TroopType troopToUpgrade, int level) throws InvalidTroopLevelException;
+    void upgradeTroop(TroopType troopToUpgrade, int level)
+        throws InvalidTroopLevelException;
 
     /**
-     * @return a map containing the player's troops with their corresponding stats
+     * @return a map containing the player's troops with their corresponding stats.
      */
-    public Map<TroopType, Integer> getTroopMap();
+    Map<TroopType, Integer> getTroopMap();
 
     /**
-     * Registers an observer object that gets notified whenever a building state changes
+     * Registers an observer object that gets notified whenever a building state changes.
      *
      * @param observer the object that needs to be registered
      */
-    public void addBuildingStateChangedObserver(final BuildingObserver observer);
+    void addBuildingStateChangedObserver(BuildingObserver observer);
 
     /**
-     * Unregisters an observer that gets notified whenever a building state changes
+     * Unregisters an observer that gets notified whenever a building state changes.
      *
      * @param observer the object that needs to be unregistered
      * @see {@link #addBuildingStateChangedObserver()}
      */
-    public void removeBuildingStateChangedObserver(final BuildingObserver observer);
+    void removeBuildingStateChangedObserver(BuildingObserver observer);
 
     /**
-     * Registers an observer object that gets notified whenever a building generates resources
+     * Registers an observer object that gets notified whenever a building generates resources.
      *
      * @param observer the object that needs to be registered
      */
-    public void addBuildingProductionObserver(final BuildingObserver observer);
+    void addBuildingProductionObserver(BuildingObserver observer);
 
     /**
-     * Unregisters an observer that gets notified whenever a building generates resources
+     * Unregisters an observer that gets notified whenever a building generates resources.
      *
      * @param observer the object that needs to be unregistered
      * @see {@link #addBuildingProductionObserver()}
      */
-    public void removeBuildingProductionObserver(final BuildingObserver observer);
+    void removeBuildingProductionObserver(BuildingObserver observer);
 
     /**
-     * Notifies all object registered to BuildingStateChangedObservers
+     * Notifies all object registered to BuildingStateChangedObservers.
      *
      * @param building the identifier of the building responsible for the event
      * @see {@link #addBuildingStateChangedObserver()}
      */
-    public void notifyBuildingStateChangedObservers(final UUID building);
+    void notifyBuildingStateChangedObservers(UUID building);
 
     /**
-     * Notifies all object registered to BuildingProductionObservers
+     * Notifies all object registered to BuildingProductionObservers.
      *
      * @param building the identifier of the building responsible for the event
      * @see {@link #addBuildingProductionObserver()}
      */
-    public void notifyBuildingProductionObservers(final UUID building);
+    void notifyBuildingProductionObservers(UUID building);
 
     /**
-     * Starts and stops the clock that keeps track of time passed
+     * Starts and stops the clock that keeps track of time passed.
      *
      * @param ticktime true to make time pass, false to stop time from passing
      */
-    public void setClockTicking(final boolean ticktime);
+    void setClockTicking(boolean ticktime);
 
     /**
-     * Checks if the clock is ticking and time is passing
+     * Checks if the clock is ticking and time is passing.
      *
      * @return true if time is passing, false if stopped
      */
-    public boolean isClockTicking();
+    boolean isClockTicking();
 
     /**
      * Returns the instance of GameData.
      * Be careful! directly manipulating data inside of the returned object
-     * might result in uncontrolled behaviour, use it carefully!
+     * might result in uncontrolled behaviour, use it carefully!.
      *
      * @return game data object that contains every information of the current game
      * @deprecated This method will soon be removed because it is unsafe and it'not the
      * BaseModel's responsibility to directly return GameData
      */
     @Deprecated
-    public GameData obtainGameData();
+    GameData obtainGameData();
 
     /**
-     * Returns an unmodifiable map of Buildings
+     * Returns an unmodifiable map of Buildings.
      *
      * @return an unmodifiable map of buildings
      */
-    public Map<UUID, Building> getBuildingMap();
+    Map<UUID, Building> getBuildingMap();
 
     /**
      * @return the player's name
      */
-    public String getPlayerName();
+    String getPlayerName();
 
     /**
-     * Sets the player's ingame name
+     * Sets the player's ingame name.
      *
      * @param playerName the player's nickname
      */
-    public void setPlayerName(String playerName);
+    void setPlayerName(String playerName);
 
     /**
-     * Safely adds or removes the given resources to the player's deposit
+     * Safely adds or removes the given resources to the player's deposit.
      *
      * @param resource A set of resources, if a resource's amount is negative, it will be subtracted from player's deposit
      */
-    public void applyResources(Set<Resource> resource) throws NotEnoughResourceException;
+    void applyResources(Set<Resource> resource) throws NotEnoughResourceException;
 
     /**
-     * Safely adds or removes the given resources to the player's deposit
+     * Safely adds or removes the given resources to the player's deposit.
      *
      * @param resource  A set of resources, if a resource's amount is negative, it will be subtracted from player's deposit
      * @param operation The type of operation that has to be applied to the resources
      */
-    public void applyResources(Set<Resource> resource, OperationType operation) throws NotEnoughResourceException;
+    void applyResources(Set<Resource> resource, OperationType operation) throws NotEnoughResourceException;
 
     /**
-     * Refreshes threadmanager's building list
+     * Refreshes threadmanager's building list.
      */
-    public void refreshBuildings();
+    void refreshBuildings();
 
     /**
-     * Applies the level multiplier given a set of resources and a level, returning a set with updated resources
+     * Applies the level multiplier given a set of resources and a level, returning a set with updated resources.
      *
      * @param resource the set of resources that need to be worked on
      * @param level    an integer representing the level
@@ -307,9 +335,10 @@ public interface BaseModel {
      * because it has been implemented internally in {@link it.unibo.model.base.internal.BuildingBuilderImpl}
      */
     @Deprecated
-    public static Set<Resource> applyMultiplierToResources(Set<Resource> resource, int level) {
+    static Set<Resource> applyMultiplierToResources(Set<Resource> resource, int level) {
         Set<Resource> alteredResource = new HashSet<>();
-        resource.forEach(singleCost -> alteredResource.add(new Resource(singleCost.getResource(), singleCost.getAmount() * (level == 0 ? 1 : level))));
+        resource.forEach(singleCost -> alteredResource.add(
+            new Resource(singleCost.getResource(), singleCost.getAmount() * (level == 0 ? 1 : level))));
         return alteredResource;
     }
 }
--- src/main/java/it/unibo/model/base/exceptions/BuildingException.java
+++ src/main/java/it/unibo/model/base/exceptions/BuildingException.java
@@ -1,11 +1,22 @@
 package it.unibo.model.base.exceptions;
 
+/**
+ * Exception that should be thrown when an operation on a building fails.
+ */
 public class BuildingException extends Exception {
-    public BuildingException(String msg) {
+    /**
+     * @see {@link Exception#Exception(String)}
+     * @param msg message
+     */
+    public BuildingException(final String msg) {
         super(msg);
     }
-
-    public BuildingException(String msg, Throwable trace) {
+    /**
+     * @see {@link Exception#Exception(String, Throwable)}
+     * @param msg message
+     * @param trace stacktrace
+     */
+    public BuildingException(final String msg, final Throwable trace) {
         super(msg, trace);
     }
 }
