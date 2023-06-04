1a0ed29b42f4fc42656421d77165bf64a1e542e4
Abdoulaye Kane <abdoulaye.kane@studio.unibo.it>
2023-06-04 18:39:05 +0200

    set the interface of the main panel for the city GUI

---
+++ src/main/java/it/unibo/view/city/CityPanel.java
@@ -0,0 +1,13 @@
+package it.unibo.view.city;
+
+import javax.swing.JPanel;
+
+public interface CityPanel {
+    public void resources();
+
+    public void setBuildings();
+
+    public void setfield();
+
+    public JPanel getPanel();
+}
