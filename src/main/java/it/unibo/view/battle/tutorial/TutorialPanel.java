88ef92b4838b8f6ae16913f3cb38c1bf4a00a55d
RealSilvi <silvi.sinani@studio.unibo.it>
2023-06-05 20:57:35 +0200

    Fixed TextPanel

--- src/main/java/it/unibo/view/battle/tutorial/TutorialPanel.java
+++ src/main/java/it/unibo/view/battle/tutorial/TutorialPanel.java
@@ -15,9 +15,7 @@ public final class TutorialPanel {
 
     private final JPanel mainPanel;
 
-    private final JPanel backPanel;
     private final JButton turnBack;
-    private final TextPanelFactoryImpl panelFactory;
 
 
     //TODO interfacce e commenti
@@ -26,25 +24,24 @@ public final class TutorialPanel {
     //TODO tutti i commenti dei costruttori
     //TODO tutti i commenti delle interfacce da rivedere
     //TODO aggiungi roba in json e fai i metodi per averli
-    public TutorialPanel() {
+    public TutorialPanel(TutorialPanelConfiguration configuration) {
         this.turnBack = new JButton(ImageIconsSupplier.getImageExitIndicator(EXIT_DIMENSION));
-        this.panelFactory = new TextPanelFactoryImpl();
-        this.backPanel = panelFactory.getTutorialSouthPanelDefault();
+        JPanel backPanel = new TextPanel(configuration.getSouthTitle(), configuration.getSouthText(), PanelDimensions.getPlayersPanel());
 
         this.turnBack.setPreferredSize(EXIT_DIMENSION);
         this.turnBack.setOpaque(false);
         this.turnBack.setBorder(BorderFactory.createLineBorder(ImageIconsSupplier.PRIMARY_COLOR, 2, true));
 
-        this.backPanel.add(this.turnBack);
+        backPanel.add(this.turnBack);
 
         this.mainPanel = new DrawPanel(Color.darkGray, PanelDimensions.SCREEN_SIZE);
         this.mainPanel.setLayout(new BorderLayout(BORDER_LAYOUT_GAP, BORDER_LAYOUT_GAP));
 
-        this.mainPanel.add(this.backPanel, BorderLayout.SOUTH);
-        this.mainPanel.add(this.panelFactory.getTutorialNorthPanelDefault(), BorderLayout.NORTH);
-        this.mainPanel.add(this.panelFactory.getTutorialWestPanelDefault(), BorderLayout.WEST);
-        this.mainPanel.add(this.panelFactory.getTutorialEastPanelDefault(), BorderLayout.EAST);
-        this.mainPanel.add(this.panelFactory.getTutorialCenterPanelDefault(), BorderLayout.CENTER);
+        this.mainPanel.add(backPanel, BorderLayout.SOUTH);
+        this.mainPanel.add(new TextPanel(configuration.getNorthTitle(),configuration.getNorthText(),PanelDimensions.getPlayersPanel()), BorderLayout.NORTH);
+        this.mainPanel.add(new TextPanel(configuration.getWestTitle(),configuration.getWestText(),PanelDimensions.getSidePanel()), BorderLayout.WEST);
+        this.mainPanel.add(new TextPanel(configuration.getEastTitle(),configuration.getEastText(),PanelDimensions.getSidePanel()), BorderLayout.EAST);
+        this.mainPanel.add(new TextPanel(configuration.getCenterTitle(),configuration.getCenterText(),PanelDimensions.getFieldPanel()), BorderLayout.CENTER);
     }
 
     public void addActionListenerExit(ActionListener actionListener) {
@@ -53,6 +50,73 @@ public final class TutorialPanel {
 
     public JPanel getPanel() {
         return this.mainPanel;
+    }
+
+    public static class TutorialPanelConfiguration{
+        private final String northTitle;
+        private final String northText;
+        private final String southTitle;
+        private final String southText;
+        private final String eastTitle;
+        private final String eastText;
+        private final String westTitle;
+        private final String westText;
+        private final String centerTitle;
+        private final String centerText;
+
+
+        public TutorialPanelConfiguration() {
+            this.northTitle = " ";
+            this.northText = " ";
+            this.southTitle = " ";
+            this.southText = " ";
+            this.eastTitle = " ";
+            this.eastText = " ";
+            this.westTitle= " ";
+            this.westText = " ";
+            this.centerTitle = " ";
+            this.centerText = " ";
+        }
+
+        public String getNorthTitle() {
+            return northTitle;
+        }
+
+        public String getNorthText() {
+            return northText;
+        }
+
+        public String getSouthTitle() {
+            return southTitle;
+        }
+
+        public String getSouthText() {
+            return southText;
+        }
+
+        public String getEastTitle() {
+            return eastTitle;
+        }
+
+        public String getEastText() {
+            return eastText;
+        }
+
+        public String getWestTitle() {
+            return westTitle;
+        }
+
+        public String getWestText() {
+            return westText;
+        }
+
+        public String getCenterTitle() {
+            return centerTitle;
+        }
+
+        public String getCenterText() {
+            return centerText;
+        }
     }
 
 }
