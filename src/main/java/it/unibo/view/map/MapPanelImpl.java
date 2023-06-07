1c4d8cf1b9077fa03a939df2ce572d2b8329bd31
Marco Francolini <marco.francolini@studio.unibo.it>
2023-06-07 19:24:32 +0200

    Improved MapPanelImpl functionality, changed MapConfiguration

--- src/main/java/it/unibo/view/map/MapPanelImpl.java
+++ src/main/java/it/unibo/view/map/MapPanelImpl.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.IntStream;
 import java.util.Map;
 import java.util.Random;
 
@@ -20,9 +21,13 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.plaf.ButtonUI;
 
 import it.unibo.controller.GameController;
+import it.unibo.view.map.internal.ButtonUIFactory;
+import it.unibo.view.map.internal.ButtonUIFactoryImpl;
 import it.unibo.view.map.internal.GraphicUtils;
+import it.unibo.view.map.mapdata.MapConfiguration;
 
 /**
  * A simple panel made of tiles that can be of different types and
@@ -32,7 +37,7 @@ public final class MapPanelImpl extends JPanel implements MapPanel {
 
     private transient Logger logger = Logger.getLogger(this.getClass().getName());
 
-    public static final int BATTLE_LEVELS = 3;
+    public static final int BATTLE_LEVELS = 55;
     public static final int RANDOM_SEED = 123545;
     public static final int ROWS = 10;
     public static final int COLS = 10;
@@ -41,6 +46,7 @@ public final class MapPanelImpl extends JPanel implements MapPanel {
     private List<JButton> tiles = new ArrayList<>();
     private Random randomGen = new Random(RANDOM_SEED);
     private transient GameController controller;
+    private transient MapConfiguration configuration = new MapConfiguration();
 
     private List<Integer> specialTileIndexes = new ArrayList<>();
 
@@ -55,17 +61,13 @@ public final class MapPanelImpl extends JPanel implements MapPanel {
     }
 
     private MapPanelImpl() {
-        JPanel internalMapPanel = new JPanel();
-        try {
-            imageMap.put(ButtonIdentification.TILE, ImageIO.read(this.getClass().getResource("/it/unibo/textures/map/tile_grass.png")));
-            imageMap.put(ButtonIdentification.PLAYER, ImageIO.read(this.getClass().getResource("/it/unibo/textures/map/base_player.png")));
-            imageMap.put(ButtonIdentification.ENEMY, ImageIO.read(this.getClass().getResource("/it/unibo/textures/map/base_enemy.png")));
-            imageMap.put(ButtonIdentification.DEATH, ImageIO.read(this.getClass().getResource("/it/unibo/textures/map/base_enemy_defeated.png")));
-        } catch (IOException | IllegalArgumentException e) {
-            logger.warning("Error loading resources!");
-            e.printStackTrace();
-            internalMapPanel.setBackground(Color.RED);
-        }
+        Arrays.stream(ButtonIdentification.values()).forEach(identification -> {
+            try {
+                imageMap.put(identification, ImageIO.read(this.getClass().getResource(configuration.getImageMap().get(identification))));
+            } catch (IOException | IllegalArgumentException e) {
+                logger.severe("Error loading resources for: "+identification.name());
+            }
+        });
         this.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
@@ -75,6 +77,7 @@ public final class MapPanelImpl extends JPanel implements MapPanel {
             }
         });
         Dimension cellSize = calculateCellSize();
+        //ButtonUI tileUI = new ButtonUIFactoryImpl().buttonUINoGrayOut();
         setLayout(new GridLayout(ROWS, COLS));
         for (int row = 0; row < ROWS; row++) {
             for (int col = 0; col < COLS; col++) {
@@ -83,7 +86,9 @@ public final class MapPanelImpl extends JPanel implements MapPanel {
                 button.setPreferredSize(cellSize);
                 button.setActionCommand(ButtonIdentification.TILE.getActionCommand());
                 button.setIcon(new ImageIcon(imageMap.get(ButtonIdentification.TILE)));
+                //button.setUI(tileUI);
                 button.setBorderPainted(false);
+                button.setContentAreaFilled(false);
                 tiles.add(button);
                 this.add(button);
             }
@@ -113,6 +118,14 @@ public final class MapPanelImpl extends JPanel implements MapPanel {
             tiles.get(specialTileIndexes.get(index))
                 .setActionCommand(ButtonIdentification.DEATH.getActionCommand());
         }
+    }
+
+    @Override
+    public void setActiveBattle(final int battleIndex) {
+        int selectedIndex = battleIndex>0 ? battleIndex : 0;
+        IntStream.range(1, specialTileIndexes.size()).forEach(specialIndex -> 
+            tiles.get(specialTileIndexes.get(specialIndex))
+                .setEnabled(specialTileIndexes.get(specialIndex) == selectedIndex));
     }
 
     private Dimension calculateCellSize() {
--- src/main/java/it/unibo/view/map/mapdata/MapConfiguration.java
+++ src/main/java/it/unibo/view/map/mapdata/MapConfiguration.java
@@ -1,12 +1,29 @@
 package it.unibo.view.map.mapdata;
 
-import java.util.List;
+import java.net.URL;
+import java.util.EnumMap;
+import java.util.Map;
 
-import it.unibo.kingdomclash.util.Pair;
+import it.unibo.view.map.MapPanel.ButtonIdentification;
 
-public class MapConfiguration {
+public final class MapConfiguration {
 
-    private List<Pair<Integer, Integer>> cityPositions;
-    private String hostileCityImageName;
-    private String playerCityImageName;
+    private Map<ButtonIdentification, String> imageMap;
+
+    public MapConfiguration() {
+        this.imageMap = new EnumMap<>(ButtonIdentification.class);
+        this.imageMap.put(ButtonIdentification.TILE,
+            "/it/unibo/textures/map/tile_grass_2.png");
+        this.imageMap.put(ButtonIdentification.PLAYER,
+            "/it/unibo/textures/map/base_player.png");
+        this.imageMap.put(ButtonIdentification.ENEMY,
+            "/it/unibo/textures/map/base_enemy.png");
+        this.imageMap.put(ButtonIdentification.DEATH,
+            "/it/unibo/textures/map/base_enemy_defeated.png");
+        
+    }
+
+    public Map<ButtonIdentification, String> getImageMap() {
+        return imageMap;
+    }
 }
