6dbed3566b9f15df99ba549137cd26a6b39f8f80
Marco Francolini <marco.francolini@studio.unibo.it>
2023-06-07 19:30:28 +0200

    Added a function to overlay an image into another in GraphicUtils

--- src/main/java/it/unibo/view/map/internal/GraphicUtils.java
+++ src/main/java/it/unibo/view/map/internal/GraphicUtils.java
@@ -1,6 +1,8 @@
 package it.unibo.view.map.internal;
 
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 /**
  * A small interface with some utilities to simplify operations with the GUI
@@ -13,8 +15,12 @@ public interface GraphicUtils {
      * @param height    height of the image
      * @return          resized image
      */
+    //The assignment is necessary
+    @SuppressWarnings("java:S1488")
     public static Image resizeImage(Image image, int width, int height) {
-        return image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        Image changedImage = width == 0 || height == 0 ? image :
+            image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return changedImage;
     }
     /**
      * Resizes the image while mantaining the aspect ratio.
@@ -23,9 +29,36 @@ public interface GraphicUtils {
      * @param height    new image height
      * @return          proportionally resized image
      */
-    public static Image resizeImageWithProportion(Image image, int width, int height) {
+    public static Image resizeImageWithProportion(final Image image,
+        final int width, final int height) {
         int checkedWidth = width >= height ? width : -1; 
         int checkedHeight = height >= width ? height : -1;
         return image.getScaledInstance(checkedWidth, checkedHeight, Image.SCALE_SMOOTH);
+    }
+
+    /**
+     * Overlays an image to the center of the original image.
+     * @param backgroundImage   the original image or background
+     * @param overlayImage      the image to overlay
+     * @return                  the original image with an applied overlay
+     */
+    public static Image overlayImages(final Image backgroundImage,
+        final Image overlayImage) {
+        int x = (backgroundImage.getWidth(null)
+            - overlayImage.getWidth(null)) / 2;
+        int y = (backgroundImage.getHeight(null)
+            - overlayImage.getHeight(null)) / 2;
+
+        BufferedImage processedImages = new BufferedImage(
+            backgroundImage.getWidth(null),
+            backgroundImage.getHeight(null),
+            BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g = processedImages.createGraphics();
+        g.drawImage(backgroundImage, 0, 0, null);
+        g.drawImage(overlayImage, x, y, null);
+        g.dispose();
+
+        return processedImages;
     }
 }
