9ea7e35e01707e96272d04950f4d625292d0c908
Abdoulaye Kane <abdoulaye.kane@studio.unibo.it>
2023-06-08 10:59:27 +0200

    Change the interface of the barpanel

--- src/main/java/it/unibo/view/city/panels/api/BarPanel.java
+++ src/main/java/it/unibo/view/city/panels/api/BarPanel.java
@@ -4,16 +4,19 @@ import java.awt.*;
 import java.awt.List;
 import java.util.*;
 
+import javax.swing.JTextArea;
+
+import it.unibo.controller.base.BaseControllerImpl;
 import it.unibo.model.data.GameData;
 import it.unibo.model.data.Resource;
-import it.unibo.view.battle.Troop;
+import it.unibo.model.data.TroopType;
 
 public interface BarPanel {
 
     void switchbutton();
 
-    void getTroopInfo(Set<Troop> type);
+    public JTextArea getInfo(Dimension size);
 
-    void getPlayerInfo(GameData gameData);
+    void getPlayerInfo(BaseControllerImpl basedata);
 
 }
