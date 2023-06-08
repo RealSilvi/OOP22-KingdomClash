2f0d92247c5b4900f1ddc6171fd021e39ef51487
Abdoulaye Kane <abdoulaye.kane@studio.unibo.it>
2023-06-08 11:00:54 +0200

    fixing the fiel troop to trooptype and  changed visibility of the getInfo method

--- src/main/java/it/unibo/view/city/panels/impl/BarPanelImpl.java
+++ src/main/java/it/unibo/view/city/panels/impl/BarPanelImpl.java
@@ -8,21 +8,24 @@ import javax.swing.*;
 
 import it.unibo.model.data.GameData;
 import it.unibo.model.data.Resource;
+import it.unibo.model.data.TroopType;
 import it.unibo.model.data.Resource.ResourceType;
-import it.unibo.view.battle.Troop;
 import it.unibo.view.battle.panels.entities.DrawPanel;
 import it.unibo.view.battle.panels.utilities.ImageIconsSupplier;
 import it.unibo.view.city.panels.api.BarPanel;
+import it.unibo.controller.base.BaseControllerImpl;
 
 public class BarPanelImpl extends JLabel implements BarPanel {
 
     private final JPanel mainpanel;
-    private Troop baseTroop;
+    private TroopType baseTroop;
     private GameData gameData;
+    private BaseControllerImpl basedata;
 
-    public BarPanelImpl(Set<Resource> resources, Set<Troop> type){
+    public BarPanelImpl(Set<Resource> resources, Set<TroopType> type){
         this.mainpanel=new DrawPanel(ImageIconsSupplier.BACKGROUND_FILL_PATTERN, getSize());
         this.gameData= gameData;
+        this.basedata=basedata;
 
         this.mainpanel.setLayout(new FlowLayout());
 
@@ -51,47 +54,32 @@ public class BarPanelImpl extends JLabel implements BarPanel {
         throw new UnsupportedOperationException("Unimplemented method 'switchbutton'");
     }
 
-    private JTextArea getInfo(Dimension size) {
+    public JTextArea getInfo(Dimension size) {
 
         JTextArea info = new JTextArea();
         info.setSize(size);
-        for (Troop itera : Troop.values()) {
-            /*info.append("\n"+itera.toString());*/
-            info.append("Name:\n" + itera.name() + "Level:" + itera.getLevel());
-        }
+       
+        basedata.requestTroopLevels().keySet().stream().forEach(
+            singletroop -> info.append(""+singletroop.name()+""
+                +basedata.requestTroopLevels().get(singletroop))
+
+            
+        );
+        
+       
         info.setEditable(false);
 
+        
         return info;
 
     }
 
     /*devo riportare una lista delle truppe con il loro nome e il livello */
-    @Override
-    public void getTroopInfo(Set<Troop> type) {
-
-        JTextArea info = new JTextArea();
-        info.setSize(new Dimension(5, 5));
-        /*rendere modificabile la lista(renderla una list?, conviene??)
-        type = EnumSet.allOf(Troop.class).forEach(Troop -> System.out.println(""+Troop.name() +""+Troop.getLevel()));*/
-        type = EnumSet.allOf(Troop.class);
-
-        Iterator troopsiterator = type.iterator();
-        while (troopsiterator.hasNext()) {
-
-            info.append("" + troopsiterator.toString());
-            System.out.println(troopsiterator);
-            troopsiterator.next();
-
-        }
-
-
-    }
 
     @Override
-    public void getPlayerInfo(GameData gamedata) {
-        System.out.println("" + gamedata.getPlayerName());
+    public void getPlayerInfo(BaseControllerImpl basedata) {
+        System.out.println(""+basedata.requestPlayerName());
         /*aggiungere il tempo trascorso?? */
-
     }
     
 
