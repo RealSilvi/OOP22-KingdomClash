91a9f2cbe6c7682d1dcd7645f897b351f769a625
Abdoulaye Kane <abdoulaye.kane@studio.unibo.it>
2023-06-10 00:32:29 +0200

    create a list that contain the coordinate of the buttons

--- src/main/java/it/unibo/view/city/panels/impl/FieldCityPanelImpl.java
+++ src/main/java/it/unibo/view/city/panels/impl/FieldCityPanelImpl.java
@@ -3,13 +3,12 @@ package it.unibo.view.city.panels.impl;
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 import it.unibo.model.base.BaseModelImpl;
+import it.unibo.model.base.internal.BuildingBuilder;
 import it.unibo.model.data.GameData;
-import it.unibo.view.battle.Troop;
-import it.unibo.view.battle.panels.api.FieldPanel;
 import it.unibo.view.battle.panels.entities.DrawPanel;
-import it.unibo.view.battle.panels.utilities.ImageIconsSupplier;
 import it.unibo.view.city.panels.api.FieldCityPanel;
 
 public class FieldCityPanelImpl implements FieldCityPanel {
@@ -17,32 +16,47 @@ public class FieldCityPanelImpl implements FieldCityPanel {
     private GameData data;
     private final JPanel mainpanel;
     private BaseModelImpl function;
+    private Map<BuildingBuilder.BuildingTypes,Image> buildingmap;
+    private Set<Set<Integer>> coordinate;
+    private List<List<JButton>> buttonmap;
+
    
 
     public FieldCityPanelImpl(int width, int height, Dimension size){
 
-        this.mainpanel= new DrawPanel(ImageIconsSupplier.BACKGROUND_FILL_PATTERN, size);
+        this.mainpanel= new DrawPanel(new ImageIcon("C:\\Users\\abdou\\OneDrive\\Immagini\\tiles\\grass.png"), size);
         this.data=data;
         this.function= function;
 
-        this.mainpanel.setLayout(new BorderLayout());
+        this.mainpanel.setLayout(new GridLayout(width,height));
+        this.setfield(width, height);
+        buildingmap=new EnumMap<>(BuildingBuilder.BuildingTypes.class);
+        buttonmap= new ArrayList<>();
+       
 
     }
 
-    @Override
-    public JPanel setfield(int width, int height) {
-        final JPanel fieldpanel= new JPanel(new GridLayout(width, height));
+    
+    private void setfield(int width, int height) {
+       
+        
         for (int i = 0; i < width; i++) {
+            List<JButton> cols= new ArrayList<>();
             for (int j = 0; j < height; j++) {
                 /*ognuna di esse ha una determinata posizione -aggiungere un listener per ciascun bottone con la quale possa piazzare la struttura 
                 */
-                final JButton structure= new JButton(""+i+","+j);
+                
+                final JButton structure= new JButton();
+                cols.add(j, structure);
                 /*il campo viene creato per caso magari aggiungo un metodo che generi immagini e le piazzi per caso */
-                structure.setIcon(new ImageIcon("C:\\Users\\abdou\\OneDrive\\Immagini\\tiles\\grass.png"));
-                fieldpanel.add(structure);
+                structure.setOpaque(false);
+                structure.setContentAreaFilled(false);
+                this.mainpanel.add(structure);
+                structure.setBorder(null);
             }
+            buttonmap.add(i, cols);
     }
-        return fieldpanel;
+        
         }
     @Override
     public void showresources() {
@@ -54,5 +68,7 @@ public class FieldCityPanelImpl implements FieldCityPanel {
         return this.mainpanel;
     }
 
+    public void getImage(){
 
+    }
 }
