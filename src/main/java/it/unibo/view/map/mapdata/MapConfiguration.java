67b4f758bdd4239e9038ecb623078d1b9cc6647a
Marco Francolini <marco.francolini@studio.unibo.it>
2023-06-09 17:50:46 +0200

    Edited default configuration for MapPanelImpl

--- src/main/java/it/unibo/view/map/mapdata/MapConfiguration.java
+++ src/main/java/it/unibo/view/map/mapdata/MapConfiguration.java
@@ -11,17 +11,18 @@ public final class MapConfiguration {
     private int columns;
     private Map<ButtonIdentification, String> imageMap;
 
+    @SuppressWarnings("java:S1075")
+    //This is a default configuration file constructor
     public MapConfiguration() {
         this.imageMap = new EnumMap<>(ButtonIdentification.class);
         this.imageMap.put(ButtonIdentification.TILE,
-            "/it/unibo/textures/map/tile_grass_2.png");
+            "/it/unibo/textures/map/grass_01.png");
         this.imageMap.put(ButtonIdentification.PLAYER,
-            "/it/unibo/textures/map/base_player_2.png");
+            "/it/unibo/textures/map/city.png");
         this.imageMap.put(ButtonIdentification.ENEMY,
-            "/it/unibo/textures/map/base_enemy_2.png");
+            "/it/unibo/textures/map/enemy.png");
         this.imageMap.put(ButtonIdentification.DEATH,
-            "/it/unibo/textures/map/base_enemy_defeated_2.png");
-
+            "/it/unibo/textures/map/ruins.png");
         this.rows = 10;
         this.columns = 10;
     }
@@ -30,13 +31,13 @@ public final class MapConfiguration {
         return imageMap;
     }
     /**
-     * @return map tile rows
+     * @return  map tile rows
      */
     public int getRows() {
         return rows;
     }
     /**
-     * @return map tile columns
+     * @return  map tile columns
      */
     public int getColumns() {
         return columns;
