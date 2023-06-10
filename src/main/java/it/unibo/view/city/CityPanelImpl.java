3e7e644d7e845d3a6f8a31820d56ee6c3fb65457
Abdoulaye Kane <abdoulaye.kane@studio.unibo.it>
2023-06-10 19:07:34 +0200

    fixed the size of the main city panel and removed the comments

--- src/main/java/it/unibo/view/city/CityPanelImpl.java
+++ src/main/java/it/unibo/view/city/CityPanelImpl.java
@@ -17,23 +17,20 @@ public class CityPanelImpl implements CityPanel{
     private final BarPanelImpl barPanel;
     private final FieldCityPanelImpl fieldPanel;
 
-    public CityPanelImpl(GameController controller, GameConfiguration configuration, Dimension size){
-        /*TO-DO */
-        /*find a background image for the wallpaper of the panel */
-        this.mainPanel=new DrawPanel(ImageIconsSupplier.BACKGROUND_FILL_PATTERN, size);
+    public CityPanelImpl(GameController controller, GameConfiguration configuration){
+       
+        this.mainPanel = new DrawPanel(ImageIconsSupplier.BACKGROUND_FILL_PATTERN,
+            new Dimension(configuration.getCityConfiguration().getWidth(),
+                configuration.getCityConfiguration().getHeight()));
         this.mainPanel.setLayout(new BorderLayout());
 
         this.barPanel=new BarPanelImpl();
         this.fieldPanel=new FieldCityPanelImpl();
 
-        //final JPanel cityPanel= new JPanel(new BorderLayout(BORDER_LAYOUT_GAP, BORDER_LAYOUT_GAP));
-        //cityPanel.setBorder(new TitledBorder("box della interfaccia"));
-
-       //cityPanel.add(barPanel.getPanel(),BorderLayout.NORTH); 
-       //cityPanel.add(fieldPanel.getpanel(),BorderLayout.CENTER);
+        
        this.mainPanel.add(barPanel.getPanel(),BorderLayout.NORTH);
        this.mainPanel.add(fieldPanel.getPanel(),BorderLayout.CENTER);
-       //this.mainPanel.add(cityPanel);
+       
         
     }
     
@@ -52,12 +49,6 @@ public class CityPanelImpl implements CityPanel{
     public JPanel getPanel(){
         return this.mainPanel;
     }
-
-    /*@Override
-    public void resources() {
-       this.fieldPanel.showresources();
-    }
-    */
 
     @Override
     public void setBuildings() {
