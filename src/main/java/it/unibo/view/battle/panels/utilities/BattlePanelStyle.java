34c4f1cdb92060a0e2b5eb6cac5dd39361c5a913
RealSilvi <silvi.sinani@studio.unibo.it>
2023-06-10 22:08:06 +0200

    implemented new way to load primary font

--- src/main/java/it/unibo/view/battle/panels/utilities/BattlePanelStyle.java
+++ src/main/java/it/unibo/view/battle/panels/utilities/BattlePanelStyle.java
@@ -3,30 +3,31 @@ package it.unibo.view.battle.panels.utilities;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public interface BattlePanelStyle {
 
-    String fontsDirectory = System.getProperty("user.dir")
-            + File.separator + "src"
-            + File.separator + "main"
-            + File.separator + "resources"
-            + File.separator + "it"
+    String fontsDirectory = File.separator + "it"
             + File.separator + "unibo"
             + File.separator + "fonts"
             + File.separator + "battle"
             + File.separator;
+
+    String primaryFontName = "armalite";
+
+    String fontExtension = ".ttf";
 
     Color PRIMARY_COLOR = new Color(168, 19, 48);
     Color SECONDARY_COLOR = new Color(250, 160, 21);
     Color DEFAULT_COLOR = Color.darkGray;
 
     static Font getPrimaryFont() {
-        float fontSize=40f;
+        float fontSize = 40f;
         Font font;
         try {
-            font = Font.createFont(Font.TRUETYPE_FONT, new File( fontsDirectory + "armalite.ttf"));
+            font = Font.createFont(Font.TRUETYPE_FONT, Objects.requireNonNull(BattlePanelStyle.class.getResourceAsStream(fontsDirectory + primaryFontName + fontExtension)));
             GraphicsEnvironment g = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            g.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(fontsDirectory +"armalite.ttf")));
+            g.registerFont(font);
             return font.deriveFont(fontSize);
         } catch (IOException | FontFormatException e) {
             font = Font.getFont("Arial");
