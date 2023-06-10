01f173ecdb1b3db319714ed6d30c0f9ec0711557
Abdoulaye Kane <abdoulaye.kane@studio.unibo.it>
2023-06-10 18:42:03 +0200

    remove unused package, add a costructor for the tests and fixed the costructor

--- src/main/java/it/unibo/view/city/CityPanelImpl.java
+++ src/main/java/it/unibo/view/city/CityPanelImpl.java
@@ -1,21 +1,13 @@
 package it.unibo.view.city;
 
 import it.unibo.controller.GameController;
-import it.unibo.model.base.BaseModel;
 import it.unibo.model.data.GameConfiguration;
-import it.unibo.model.data.GameData;
-import it.unibo.model.data.Resource;
-import it.unibo.model.data.TroopType;
-import it.unibo.model.data.Resource.ResourceType;
 import it.unibo.view.battle.panels.entities.DrawPanel;
 import it.unibo.view.battle.panels.utilities.ImageIconsSupplier;
 import it.unibo.view.city.panels.impl.BarPanelImpl;
 import it.unibo.view.city.panels.impl.FieldCityPanelImpl;
 
 import javax.swing.*;
-import javax.swing.border.TitledBorder;
-
-import java.util.*;
 import java.awt.*;
 
 public class CityPanelImpl implements CityPanel{
@@ -25,7 +17,7 @@ public class CityPanelImpl implements CityPanel{
     private final BarPanelImpl barPanel;
     private final FieldCityPanelImpl fieldPanel;
 
-    public CityPanelImpl(GameController controller, GameConfiguration configuration){
+    public CityPanelImpl(GameController controller, GameConfiguration configuration, Dimension size){
         /*TO-DO */
         /*find a background image for the wallpaper of the panel */
         this.mainPanel=new DrawPanel(ImageIconsSupplier.BACKGROUND_FILL_PATTERN, size);
@@ -40,19 +32,32 @@ public class CityPanelImpl implements CityPanel{
        //cityPanel.add(barPanel.getPanel(),BorderLayout.NORTH); 
        //cityPanel.add(fieldPanel.getpanel(),BorderLayout.CENTER);
        this.mainPanel.add(barPanel.getPanel(),BorderLayout.NORTH);
-       this.mainPanel.add(fieldPanel.getpanel(),BorderLayout.CENTER);
+       this.mainPanel.add(fieldPanel.getPanel(),BorderLayout.CENTER);
        //this.mainPanel.add(cityPanel);
         
     }
     
+    public CityPanelImpl() {
+        this.mainPanel=new DrawPanel(ImageIconsSupplier.BACKGROUND_FILL_PATTERN, new Dimension(10, 10));
+        this.mainPanel.setLayout(new BorderLayout());
+
+        this.barPanel=new BarPanelImpl();
+        this.fieldPanel=new FieldCityPanelImpl();
+        
+       
+       this.mainPanel.add(barPanel.getPanel(),BorderLayout.NORTH);
+       this.mainPanel.add(fieldPanel.getPanel(),BorderLayout.CENTER);
+    }
+
     public JPanel getPanel(){
         return this.mainPanel;
     }
 
-    @Override
+    /*@Override
     public void resources() {
        this.fieldPanel.showresources();
     }
+    */
 
     @Override
     public void setBuildings() {
@@ -64,6 +69,12 @@ public class CityPanelImpl implements CityPanel{
     public void setfield() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setfield'");
+    }
+
+    @Override
+    public void resources() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resources'");
     }
 
    
