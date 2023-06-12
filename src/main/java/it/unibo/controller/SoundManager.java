14064ff5f65b902fb55d188eac7a758e2b155de5
RealSilvi <silvi.sinani@studio.unibo.it>
2023-06-12 19:43:37 +0200

    Fixed SoundManager

--- src/main/java/it/unibo/controller/SoundManager.java
+++ src/main/java/it/unibo/controller/SoundManager.java
@@ -2,9 +2,9 @@ package it.unibo.controller;
 
 
 import javax.sound.sampled.*;
-import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -13,14 +13,8 @@ import java.util.Optional;
 public class SoundManager {
 
     private static final String SOUND_FILE_EXTENSION = ".wav";
-    private static final String SOUND_THEMES_DIRECTORY = System.getProperty("user.dir")
-            + File.separator + "src"
-            + File.separator + "main"
-            + File.separator + "resources"
-            + File.separator + "it"
-            + File.separator + "unibo"
-            + File.separator + "soundThemes"
-            + File.separator;
+    private static final String SOUND_THEMES_DIRECTORY = "/it/unibo/soundThemes/";
+
 
     private static final String MENU_THEME_PATH = SOUND_THEMES_DIRECTORY + "menuTheme" + SOUND_FILE_EXTENSION;
     private static final String CITY_THEME_PATH = SOUND_THEMES_DIRECTORY + "cityTheme" + SOUND_FILE_EXTENSION;
@@ -42,10 +36,10 @@ public class SoundManager {
         this.currentTheme= Optional.empty();
         this.enable=true;
         this.themesClips = Map.of(
-                Themes.MENU, this.createClip(new File(MENU_THEME_PATH)),
-                Themes.BATTLE, this.createClip(new File(BATTLE_THEME_PATH)),
-                Themes.CITY, this.createClip(new File(CITY_THEME_PATH)),
-                Themes.MAP, this.createClip(new File(MAP_THEME_PATH))
+                Themes.MENU, this.createClip(MENU_THEME_PATH),
+                Themes.BATTLE, this.createClip(BATTLE_THEME_PATH),
+                Themes.CITY, this.createClip(CITY_THEME_PATH),
+                Themes.MAP, this.createClip(MAP_THEME_PATH)
         );
     }
 
@@ -93,9 +87,9 @@ public class SoundManager {
         }
     }
 
-    private Optional<Clip> createClip(final File themeFile) {
+    private Optional<Clip> createClip(final String themeFile) {
         try {
-            final AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(themeFile);
+            final AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(Objects.requireNonNull(SoundManager.class.getResourceAsStream(themeFile)));
             final Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
             return Optional.of(clip);
@@ -103,44 +97,5 @@ public class SoundManager {
             return Optional.empty();
         }
     }
-
-
-
-//    public static void zombiesAreComing() {
-//        playSoundEffect(ResourceFinder.getSoundURL(ZOMBIES_SIREN_FILE_NAME));
-//    }
-
-
-//    private static void playLoopSound(final URL soundUrl) {
-//        final Optional<Clip> clip = createClip(soundUrl, MAIN_THEME_VOLUME);
-//        clip.ifPresent(c -> {
-//            c.start();
-//            clip.get().loop(Clip.LOOP_CONTINUOUSLY);
-//        });
-//    }
-
-//    private static void playSoundEffect(final URL soundUrl) {
-//        final Optional<Clip> clip = createClip(soundUrl, SOUND_EFFECT_VOLUME);
-//        clip.ifPresent(Clip::start);
-//    }
-
-//    private Optional<Clip> createClip(final URL soundUrl, final float volume) {
-//        try {
-//            final AudioInputStream ais = AudioSystem.getAudioInputStream(soundUrl);
-//            final Clip clip = AudioSystem.getClip();
-//            clip.addLineListener(event -> {
-//                if (LineEvent.Type.STOP.equals(event.getType())) {
-//                    clip.close();
-//                }
-//            });
-//            clip.open(ais);
-//            final FloatControl fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-//            fc.setValue(volume);
-//
-//            return Optional.of(clip);
-//        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
-//            return Optional.empty();
-//        }
-//    }
 
 }
