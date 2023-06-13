568251263521f2b223353b49b98c9f33f770aef7
Abdoulaye Kane <abdoulaye.kane@studio.unibo.it>
2023-06-13 12:27:35 +0200

    add the texture to the city configuration

--- src/main/java/it/unibo/view/city/panels/impl/CityConfiguration.java
+++ src/main/java/it/unibo/view/city/panels/impl/CityConfiguration.java
@@ -29,9 +29,15 @@ public class CityConfiguration {
         this.height=10;
 
         textureMap=new EnumMap<>(BuildingBuilder.BuildingTypes.class);
-        textureMap.put(BuildingTypes.FARM,Map.of(0,"",1,"",2,""));
-        textureMap.put(BuildingTypes.HALL, Map.of(0, "", 1, "", 2, ""));
-        textureMap.put(BuildingTypes.LUMBERJACK, Map.of(0, "", 1, "", 2, ""));
+        textureMap.put(BuildingTypes.FARM,Map.of(0,"KingdomClash\\src\\main\\resources\\it\\unibo\\textures\\city\\wooden_buildings_01_1.png",
+            1,"\\KingdomClash\\src\\main\\resources\\it\\unibo\\textures\\city\\wooden_buildings_01_2.png",
+                2,"\\KingdomClash\\src\\main\\resources\\it\\unibo\\textures\\city\\wooden_buildings_01_4.png"));
+        textureMap.put(BuildingTypes.HALL, Map.of(0, "C:\\Users\\abdou\\Downloads\\texture\\yusuf-artun-map-buildings-1_3-removebg-preview.png",
+             1,"C:\\Users\\abdou\\Downloads\\texture\\yusuf-artun-map-buildings-1_2-removebg-preview.png",
+                 2, "C:\\Users\\abdou\\Downloads\\texture\\yusuf-artun-map-buildings-1_1-removebg-preview.png"));
+        textureMap.put(BuildingTypes.LUMBERJACK, Map.of(0,"KingdomClash\\src\\main\\resources\\it\\unibo\\textures\\city\\wooden_buildings_01_13.png",
+             1,"KingdomClash\\src\\main\\resources\\it\\unibo\\textures\\city\\wooden_buildings_01_14.png",
+              2,"KingdomClash\\src\\main\\resources\\it\\unibo\\textures\\city\\wooden_buildings_01_15.png"));
 
     }
 
