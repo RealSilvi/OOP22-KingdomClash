8a1c0a370b847e856d9a68712edc9763679d91ec
Abdoulaye Kane <abdoulaye.kane@studio.unibo.it>
2023-06-14 23:48:00 +0200

    create class for the popup

---
+++ src/main/java/it/unibo/view/city/utilities/ResourcePopupPanel.java
@@ -0,0 +1,36 @@
+package it.unibo.view.city.utilities;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Popup;
+import javax.swing.PopupFactory;
+
+public class ResourcePopupPanel {
+    private Popup popup;
+    private final int width;
+    private final int height;
+    private JPanel contentpanel;
+    private boolean visibility;
+
+    public ResourcePopupPanel(Component container, int xposition,int yposition) {
+        this.width = 200;
+        this.height = 200;
+        this.visibility = false;
+        this.contentpanel = new JPanel();
+        this.contentpanel.add(new JLabel("ResourcePopupPanel"));
+        this.contentpanel.setPreferredSize(new Dimension(this.width, this.height));
+        this.popup = new PopupFactory().getPopup(container, contentpanel, xposition, yposition);
+    }
+
+    public void changeVisibility(){
+        if(visibility) {
+            popup.show();
+        } else {
+            popup.hide();
+        }
+    }
+}
