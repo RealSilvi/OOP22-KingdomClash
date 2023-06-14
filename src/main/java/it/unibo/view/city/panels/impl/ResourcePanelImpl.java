6ba81e0d7795b535952d9ecc0399a6e154032201
Marco Francolini <marco.francolini@studio.unibo.it>
2023-06-14 23:50:36 +0200

    Removed unused functionality in ResourcePanelImpl

--- src/main/java/it/unibo/view/city/panels/impl/ResourcePanelImpl.java
+++ src/main/java/it/unibo/view/city/panels/impl/ResourcePanelImpl.java
@@ -6,14 +6,11 @@ import java.util.Map;
 import java.util.UUID;
 
 import javax.swing.BoxLayout;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import it.unibo.controller.base.BaseController;
-import it.unibo.controller.base.BaseControllerImpl;
 import it.unibo.model.base.api.BuildingObserver;
-import it.unibo.model.data.GameData;
 import it.unibo.model.data.Resource.ResourceType;
 
 public class ResourcePanelImpl extends JPanel {
@@ -41,21 +38,5 @@ public class ResourcePanelImpl extends JPanel {
                 }
             }
         });
-    }
-
-    /**
-     * Opens the panel inside a JFrame, for manual testing purposes only.
-     */
-    public void showInJFrame() {
-        JFrame frame = new JFrame("MapPanelTestGUI");
-        frame.setLayout(null);
-        frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(1920, 1080);
-        frame.add(this);
-        frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        new ResourcePanelImpl(new BaseControllerImpl(new GameData())).showInJFrame();
     }
 }
