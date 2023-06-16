56babf461309f76217c0da465f8357a9b6a9c571
Marco Francolini <marco.francolini@studio.unibo.it>
2023-06-16 14:29:44 +0200

    Added observer functionality in CityPanel

--- src/main/java/it/unibo/view/city/CityPanel.java
+++ src/main/java/it/unibo/view/city/CityPanel.java
@@ -1,8 +1,12 @@
 package it.unibo.view.city;
 
 import java.awt.event.ActionListener;
+import java.awt.geom.Point2D;
 
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+
+import it.unibo.view.city.panels.api.TileClickObserver;
 
 public interface CityPanel {
      void resources();
@@ -17,5 +21,21 @@ public interface CityPanel {
       * @param returnActionListener action listener to assign to this view
       */
      void setReturnActionListener(ActionListener returnActionListener);
+     /**
+      * Registers an observers that gets notified whenever a tile gets clicked.
+      * @param tileClickObservertoRegister the observer to register
+      */
+     void registerTileClickObserver(TileClickObserver tileClickObservertoRegister);
+     /**
+      * Unregisters an observers that gets notified whenever a tile gets clicked.
+      * @param tileClickObservertoRegister the observer to unregister
+      */
+     void unregisterTileClickObserver(TileClickObserver tileClickObservertoUnregister);
+     /**
+      * Notifies all the registered tileClickObservers.
+      * @param tile      the responsible tile as a JComponent
+      * @param position  the responsible position in the tilemap
+      */
+     void notifyTileClick(JComponent tile, Point2D.Float position);
 }
 
--- src/main/java/it/unibo/view/city/CityPanelImpl.java
+++ src/main/java/it/unibo/view/city/CityPanelImpl.java
@@ -6,16 +6,25 @@ import it.unibo.model.data.GameConfiguration;
 import it.unibo.view.GameGui;
 import it.unibo.view.battle.config.PathIconsConfiguration;
 import it.unibo.view.battle.panels.entities.DrawPanel;
+import it.unibo.view.city.panels.api.TileClickObserver;
 import it.unibo.view.city.panels.impl.BarPanelImpl;
 import it.unibo.view.city.panels.impl.FieldCityPanelImpl;
 import it.unibo.view.utilities.ImageIconsSupplier;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.event.ActionListener;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 public class CityPanelImpl implements CityPanel {
 
@@ -26,9 +35,12 @@ public class CityPanelImpl implements CityPanel {
     private final FieldCityPanelImpl fieldPanel;
     private final Map<BuildingTypes, Map<Integer, Image>> readImages = new EnumMap<>(BuildingTypes.class);
 
+    private List<TileClickObserver> tileClickObservers;
+
     private ActionListener returnActionListener;
 
     public CityPanelImpl(BaseControllerImpl controller, GameConfiguration configuration) {
+        this.tileClickObservers = new ArrayList<>();
         PathIconsConfiguration config = configuration.getPathIconsConfiguration();
         for (BuildingTypes buildingType : BuildingTypes.values()) {
             Map<Integer, Image> imageLevel = new HashMap<>();
@@ -38,15 +50,14 @@ public class CityPanelImpl implements CityPanel {
             readImages.put(buildingType, imageLevel);
         }
 
-        
         this.mainPanel = new DrawPanel(Color.BLACK,
                 new Dimension(configuration.getCityConfiguration().getWidth(),
                         configuration.getCityConfiguration().getHeight()));
         this.mainPanel.setLayout(new BorderLayout());
 
-        this.barPanel = new BarPanelImpl(controller, size, readImages);
-        this.fieldPanel = new FieldCityPanelImpl(configuration.getCityConfiguration(), configuration.getPathIconsConfiguration());
-
+        this.barPanel = new BarPanelImpl(this, controller, size, readImages);
+        this.fieldPanel = new FieldCityPanelImpl(this,
+            configuration.getCityConfiguration(), configuration.getPathIconsConfiguration());
 
         this.mainPanel.add(barPanel.getPanel(), BorderLayout.NORTH);
         this.mainPanel.add(fieldPanel.getPanel(), BorderLayout.CENTER);
@@ -81,5 +92,21 @@ public class CityPanelImpl implements CityPanel {
 
     public JPanel getPanel() {
         return this.mainPanel;
+    }
+
+    @Override
+    public void registerTileClickObserver(TileClickObserver tileClickObservertoRegister) {
+        this.tileClickObservers.add(tileClickObservertoRegister);
+    }
+
+    @Override
+    public void unregisterTileClickObserver(TileClickObserver tileClickObservertoUnregister) {
+        this.tileClickObservers.remove(tileClickObservertoUnregister);
+    }
+
+    @Override
+    public void notifyTileClick(JComponent tile, Point2D.Float position) {
+        this.tileClickObservers.stream().forEach(tileObserver -> 
+            tileObserver.tileClicked(tile, position));
     }
 }
