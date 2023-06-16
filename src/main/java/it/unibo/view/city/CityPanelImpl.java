4f59b13052b013f088a9477998e2d1a11ca60420
Marco Francolini <marco.francolini@studio.unibo.it>
2023-06-16 17:59:12 +0200

    Fixed error in CityPanelImpl

--- src/main/java/it/unibo/view/city/CityPanelImpl.java
+++ src/main/java/it/unibo/view/city/CityPanelImpl.java
@@ -64,7 +64,7 @@ public class CityPanelImpl implements CityPanel {
                         configuration.getCityConfiguration().getHeight()));
         this.mainPanel.setLayout(new BorderLayout());
 
-        this.barPanel = new BarPanelImpl(this, controller, size, readImages);
+        this.barPanel = new BarPanelImpl(this, controller, SIZE, readImages);
         this.fieldPanel = new FieldCityPanelImpl(this, controller,
             configuration, configuration.getCityConfiguration(), configuration.getPathIconsConfiguration(), this.readImages);
 
