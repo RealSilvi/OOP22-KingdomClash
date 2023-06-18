c590b68ff286a105446b05a3d3ab681125523d84
RealSilvi <silvi.sinani@studio.unibo.it>
2023-06-18 23:55:26 +0200

    Added javadoc and moved path images logics from MapConfiguration to PathIconsConfiguration

--- src/main/java/it/unibo/kingdomclash/config/TextConfigurationBattle.java
+++ src/main/java/it/unibo/kingdomclash/config/TextConfigurationBattle.java
@@ -1,6 +1,9 @@
 package it.unibo.kingdomclash.config;
 
-public  class TextConfiguration{
+/**
+ * Configuration of text areas in the battle.
+ */
+public class TextConfigurationBattle {
 
     private final String tutorialNorthTitle;
     private final String tutorialNorthText;
@@ -18,75 +21,117 @@ public  class TextConfiguration{
     private final String endLosePanelText;
 
 
-    public TextConfiguration() {
+    public TextConfigurationBattle() {
         this.tutorialNorthTitle = " ";
         this.tutorialNorthText = "This panel contains the opponent's set of dice.";
         this.tutorialSouthTitle = " ";
         this.tutorialSouthText = " In this panel, you'll find your set of data, where you can select troops to deploy on the battlefield. The chosen troops are automatically arranged on the battlefield.";
         this.tutorialEastTitle = " ";
         this.tutorialEastText = "This panel provides information about the troops currently on the battlefield. ";
-        this.tutorialWestTitle= " ";
+        this.tutorialWestTitle = " ";
         this.tutorialWestText = "Here, the combat effectiveness of each troop is compared to the level of the opponent's troops. If two troops of the same level clash, they cancel each other out. However, if one troop has a higher level than the other, it prevails over the other. ";
         this.tutorialCenterTitle = " ";
         this.tutorialCenterText = "Within this panel, you'll find the battlefield,where deployed troops are automatically arranged. The outcome of the combat between one troop and another is displayed in the information panel. ";
         this.endWinPanelTitle = "YOU WIN";
-        this.endWinPanelText=" ";
+        this.endWinPanelText = " ";
         this.endLosePanelTitle = "YOU LOSE ";
         this.endLosePanelText = "";
     }
 
+    /**
+     * @return the title of the north Panel of Tutorial.
+     */
     public String getTutorialNorthTitle() {
         return tutorialNorthTitle;
     }
 
+    /**
+     * @return the text of the north Panel of Tutorial.
+     */
     public String getTutorialNorthText() {
         return tutorialNorthText;
     }
 
+    /**
+     * @return the title of the south Panel of Tutorial.
+     */
     public String getTutorialSouthTitle() {
         return tutorialSouthTitle;
     }
 
+    /**
+     * @return the text of the south Panel of Tutorial.
+     */
     public String getTutorialSouthText() {
         return tutorialSouthText;
     }
 
+    /**
+     * @return the title of the east Panel of Tutorial.
+     */
     public String getTutorialEastTitle() {
         return tutorialEastTitle;
     }
 
+    /**
+     * @return the text of the east Panel of Tutorial.
+     */
     public String getTutorialEastText() {
         return tutorialEastText;
     }
 
+    /**
+     * @return the title of the west Panel of Tutorial.
+     */
     public String getTutorialWestTitle() {
         return tutorialWestTitle;
     }
 
+    /**
+     * @return the text of the west Panel of Tutorial.
+     */
     public String getTutorialWestText() {
         return tutorialWestText;
     }
 
+    /**
+     * @return the title of the center Panel of Tutorial.
+     */
     public String getTutorialCenterTitle() {
         return tutorialCenterTitle;
     }
 
+    /**
+     * @return the text of the Center Panel of Tutorial.
+     */
     public String getTutorialCenterText() {
         return tutorialCenterText;
     }
 
+    /**
+     * @return the title of the end win Panel of Tutorial.
+     */
     public String getEndWinPanelTitle() {
         return endWinPanelTitle;
     }
 
+    /**
+     * @return the title of the end lose Panel of Tutorial.
+     */
     public String getEndLosePanelTitle() {
         return endLosePanelTitle;
     }
 
+    /**
+     * @return the text of the end win Panel of Tutorial.
+     */
     public String getEndWinPanelText() {
         return endWinPanelText;
     }
 
+    /**
+     * @return the text of the end lose Panel of Tutorial.
+     */
     public String getEndLosePanelText() {
         return endLosePanelText;
     }
--- src/main/java/it/unibo/view/city/panels/impl/FieldCityPanelImpl.java
+++ src/main/java/it/unibo/view/city/panels/impl/FieldCityPanelImpl.java
@@ -2,8 +2,6 @@ package it.unibo.view.city.panels.impl;
 
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.geom.Point2D;
@@ -39,18 +37,16 @@ public class FieldCityPanelImpl implements FieldCityPanel {
     private final CityPanel cityView;
     private final BaseController baseController;
     private final Map<BuildingTypes, Map<Integer, Image>> readImages;
-    private List<List<JButton>> buttonmap;
-    private CityConfiguration gameConfiguration;
-    private Map<UUID, Point2D> buildingTilePositions;
+    private final List<List<JButton>> buttonmap;
+    private final CityConfiguration gameConfiguration;
+    private final Map<UUID, Point2D> buildingTilePositions;
 
     /**
-     * The costructor create the panel and set the background of the field.
+     * The constructor create the panel and set the background of the field.
      *
      * @param cityView
      * @param baseController
      * @param gameConfig
-     * @param gameConfiguration      gave the width and height for the field
-     * @param pathIconsConfiguration gave the textures of the building and of the background for the field
      * @param readImages             a for each building level gave his texture
      */
     public FieldCityPanelImpl(final CityPanel cityView,
@@ -63,7 +59,7 @@ public class FieldCityPanelImpl implements FieldCityPanel {
         this.readImages = readImages;
         this.gameConfiguration = gameConfig.getCityConfiguration();
         this.mainpanel = new DrawPanelImpl(ImageIconsSupplier.loadImage(gameConfig
-                .getMapConfiguration()
+                .getPathIconsConfiguration()
                 .getImageMap().get(ButtonIdentification.TILE)),
                 GameGui.getAllPanel());
         this.mainpanel.setLayout(
@@ -73,12 +69,12 @@ public class FieldCityPanelImpl implements FieldCityPanel {
         this.setfield(gameConfiguration.getWidth(), gameConfiguration.getHeight());
         this.baseController.addBuildingStateChangedObserver(this::updateBuildingOnField);
         this.baseController.requestBuildingMap()
-            .keySet().stream().forEach(this::updateBuildingOnField);
+            .keySet().forEach(this::updateBuildingOnField);
         this.mainpanel.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(final ComponentEvent e) {
                 baseController.requestBuildingMap()
-                    .keySet().stream()
+                    .keySet()
                     .forEach(FieldCityPanelImpl.this::updateBuildingOnField);
             }
         });
@@ -98,13 +94,10 @@ public class FieldCityPanelImpl implements FieldCityPanel {
                 this.mainpanel.add(structure);
                 structure.setBorder(null);
                 final int coordY = j;
-                structure.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(final ActionEvent e) {
-                        if (e.getSource() instanceof JComponent) {
-                            cityView.notifyTileClick((JComponent) e.getSource(),
-                                    new Point2D.Float(coordX, coordY));
-                        }
+                structure.addActionListener(e -> {
+                    if (e.getSource() instanceof JComponent) {
+                        cityView.notifyTileClick((JComponent) e.getSource(),
+                                new Point2D.Float(coordX, coordY));
                     }
                 });
             }
@@ -122,14 +115,14 @@ public class FieldCityPanelImpl implements FieldCityPanel {
     private void updateBuildingOnField(final UUID buildingToUpdate) {
         BuildingTypes type;
         int level;
-        Double xPos;
-        Double yPos;
+        double xPos;
+        double yPos;
         JButton tile;
         if (!this.baseController.requestBuildingMap()
                 .containsKey(buildingToUpdate)) {
             xPos = this.buildingTilePositions.get(buildingToUpdate).getX();
             yPos = this.buildingTilePositions.get(buildingToUpdate).getY();
-            tile = this.buttonmap.get(xPos.intValue()).get(yPos.intValue());
+            tile = this.buttonmap.get((int) xPos).get((int) yPos);
             tile.setIcon(null);
         } else {
             if (!this.baseController.requestBuildingMap().get(buildingToUpdate).isBeingBuilt()) {
@@ -141,7 +134,7 @@ public class FieldCityPanelImpl implements FieldCityPanel {
                         this.baseController
                                 .requestBuildingMap()
                                 .get(buildingToUpdate).getStructurePos());
-                tile = this.buttonmap.get(xPos.intValue()).get(yPos.intValue());
+                tile = this.buttonmap.get((int) xPos).get((int) yPos);
                 tile.setIcon(new ImageIcon(GraphicUtils.resizeImageWithProportion(this.readImages.get(type).get(level),
                     tile.getWidth(), tile.getHeight())));
             }
