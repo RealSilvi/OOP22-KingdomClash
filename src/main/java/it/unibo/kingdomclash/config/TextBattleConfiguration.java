ae7eb9d7ed109136a2f542d8a674b660737d24be
RealSilvi <silvi.sinani@studio.unibo.it>
2023-08-13 12:56:13 +0200

    Update tutorial text

--- src/main/java/it/unibo/kingdomclash/config/TextBattleConfiguration.java
+++ src/main/java/it/unibo/kingdomclash/config/TextBattleConfiguration.java
@@ -24,25 +24,28 @@ public class TextBattleConfiguration {
      * Set's the default configuration.
      */
     public TextBattleConfiguration() {
-        this.tutorialNorthTitle = " ";
-        this.tutorialNorthText = "This panel contains the opponent's set of dice.";
-        this.tutorialSouthTitle = " ";
-        this.tutorialSouthText = " In this panel, you'll find your set of data,"
-                + " where you can select troops to deploy on the battlefield. "
-                + "The chosen troops are automatically arranged on the battlefield.";
-        this.tutorialEastTitle = " ";
-        this.tutorialEastText = "This panel provides information about the "
-                + "troops currently on the battlefield. ";
-        this.tutorialWestTitle = " ";
-        this.tutorialWestText = "Here, the combat effectiveness of each troop "
+        this.tutorialNorthTitle = "Enemy Troops.";
+        this.tutorialNorthText = "You can see which troops the enemy can choose.";
+        this.tutorialSouthTitle = "Player Troops.";
+        this.tutorialSouthText = "Every round the unlocked slots will refresh and "
+                + "give you the possibility to choose new troops which will add to the Battle Field."
+                + "When you click on a troop the slot will be locked until the battle ends."
+                + "When the battle ends all the slots will unlocked and refresh.";
+        this.tutorialWestTitle = "Power Table.";
+        this.tutorialWestText = "In this table you can see if you troops are enough "
+                + "powerful to win every comparison.";
+        this.tutorialEastTitle = "Main Info.";
+        this.tutorialEastText = "Here you see your life and enemies life. ì"
+                + "When somebody finish his lives the entire battle ends and there's a winner.";
+        this.tutorialCenterTitle = "Battle Field.";
+        this.tutorialCenterText = "Every round the troops you'll choose will be added in the Battle Field."
+                + "Every 3 rounds or when both the players doesnt have more possibility to chose new troops "
+                + "the battle will start." + "The combat effectiveness of each troop "
                 + "is compared to the level of the opponent's troops. "
                 + "If two troops of the same level clash, they cancel each other out. "
                 + "However, if one troop has a higher level than the other, "
-                + "it prevails over the other. ";
-        this.tutorialCenterTitle = " ";
-        this.tutorialCenterText = "Within this panel, you'll find the battlefield"
-                + ",where deployed troops are automatically arranged. "
-                + "The outcome of the combat between one troop and another "
+                + "it prevails over the other. "
+                + "The outcome of the combat and the relations between one troop and another "
                 + "is displayed in the information panel. ";
         this.endWinPanelTitle = "YOU WIN";
         this.endWinPanelText = " ";
